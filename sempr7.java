4451099f12792e0363be6159f637fbc26c9736a7
S <s>
2023-10-03 18:53:09 +0400

    {"types":"C","runs":3}

--- sempr7.java
+++ sempr7.java
@@ -1,4 +1,31 @@
+import stanford.karel.Karel;
 
-public class sempr7 {
+public class sempr7 extends Karel{
+//7. კარელი დგას 1x1 უჯრაზე, შეავსებინეთ მას მთელი სამყარო თითო ბრილიანტით. ანუ მთელ
+//სამყაროში ყველა უჯრაზე უნდა იდოს ზუსტად ერთი ბრილიანტი. ჩათვალეთ რომ საწყისი
+//სამყარო ცარიელი არ არის და გარკვეულ(ჩვენთვის უცნობ) უჯრებში თითო(მხოლოდ ერთი)
+//ბრილიანტი დევს. ამასთან გაითვალისწინეთ, რომ სამყაროს ზომები თქვენთვის უცნობია და
+//თქვენი პროგრამა უნდა მუშაობდეს ნებისმიერი ზომის სამყაროსათვის.
+	
+	public void run(){
+		
+		while (beepersInBag()){ // next line exists
+//			go to next line start position
+	//		goToNextLineStart();
+			fillLine();
+		}
+	}
+	private void fillLine(){
+		if(noBeepersPresent()){
+			putBeeper();
+		}
+		while(frontIsClear()){
+			move();
+			if(noBeepersPresent()){
+				putBeeper();
+			}
+		}
+	}
+	}
 
-}
+
