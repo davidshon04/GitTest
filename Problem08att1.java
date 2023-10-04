0337e8574aa799c56146b396fc7d100c1e8da2c6
S <s>
2023-10-04 22:20:58 +0400

    {"types":"C","runs":6}

--- Problem08att1.java
+++ Problem08att1.java
@@ -1,4 +1,36 @@
+import stanford.karel.Karel;
 
-public class Problem08att1 {
+//8. კარელმა უნდა გამოაკლოს ორი რიცხვი, 2x1 უჯრაზე დევს n ცალი ბრილიანტი, 3x1 უჯრაზე
+//m ცალი ბრილიანტი, კარელმა 4x1 უჯრაზე უნდა დადოს n-m ბრილიანტი. ჩათვალეთ, რომ
+//კარელს ჩანთაში უსასრულო რაოდენობის ბრილიანტი აქვს და ჩათვალეთ, რომ n>=m.
 
+public class Problem08att1 extends Karel{
+	
+	public void run(){
+		for(int i=0; i<2; i++){
+			move();
+		}
+		carryMBeepers();
+		//carryNMinusM();
+		//bringBackBeepers();
+	}
+
+	private void carryMBeepers(){
+		while(beepersPresent()){
+			pickBeeper();
+			turnAround();
+			move();
+			pickBeeper();
+			move();
+			putBeeper();
+			turnAround();
+			move();
+			move();
+		}
+	}
+	
+	private void turnAround(){
+		turnLeft();
+		turnLeft();
+	}
 }
