3ddb8f7e6ce2d78cbcc75647699a5cc0a5c90ae6
S <s>
2023-10-04 23:44:55 +0400

    {"types":"C","runs":1}

--- Problem09att1.java
+++ Problem09att1.java
@@ -1,4 +1,45 @@
+import stanford.karel.Karel;
 
-public class Problem09att1 {
+//9. კარელმა უნდა გაამრავლოს ორი რიცხვი, 2x1 უჯრაზე დევს n ცალი ბრილიანტი, 3x1 უჯრაზე
+//m ცალი ბრილიანტი, კარელმა 4x1 უჯრაზე უნდა დადოს n * m ბრილიანტი. ჩათვალეთ, რომ
+//კარელს ჩანთაში უსასრულო რაოდენობის ბრილიანტი აქვს.
 
+public class Problem09att1 extends Karel{
+
+	public void run(){
+		move();
+		createTable();
+		//bringBackStartingBeepers();
+		//putMultipliedBeepers();
+	}
+	
+	private void createTable(){
+		while(beepersPresent()){
+			pickBeeper();
+			move();
+			move();
+			putBeeper();
+			turnAround();
+			move();
+			turnRight();
+			while(beepersPresent()){
+				pickBeeper();
+				for(int i=0; i<2; i++){
+					move();
+					putBeeper();
+				}
+			}
+		}
+	}
+	
+	private void turnAround(){
+		turnLeft();
+		turnLeft();
+	}
+	
+	private void turnRight(){
+		for(int i=0; i<3; i++){
+			turnLeft();
+		}
+	}
 }
