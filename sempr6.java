05b626772e7bd4a37555cefd506306620e9b2314
S <s>
2023-10-05 18:29:32 +0400

    {"types":"C","runs":1}

--- sempr6.java
+++ sempr6.java
@@ -1,4 +1,33 @@
+import stanford.karel.SuperKarel;
 
-public class sempr6 {
+public class sempr6 extends SuperKarel{
+	
+	public void run(){
+		moveFirstPileToDestinationAndBackup();
+		moveSecondPileToDestinationAndBackup();
+	//	moveToSecondPile();
+		restoreBeepers();
+	//	moveToFirstPile();
+		restoreBeepers();
+	}
 
-}
+	private void moveFirstPileToDestinationAndBackUp(){
+		while(beepersPresent()){
+			pickBeeper();
+			moveBeeperFromFirstPileToDestinationPile();
+			backupBeeper();
+		}
+	}
+
+	private void moveBeeperFromFirstPileToDestinationPile() {
+		move();
+		move();
+		putBeeper();
+		turnAround();
+		move();
+		move();
+		turnAround();
+		
+	}
+	
+}	
