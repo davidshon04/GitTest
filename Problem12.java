a0cfa8cbe95d41a0fb4007643a91b9282489f385
S <s>
2023-10-10 18:54:23 +0400

    {"types":"C","runs":15}

--- Problem12.java
+++ Problem12.java
@@ -1,4 +1,12 @@
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
-public class Problem12 {
+public class Problem12 extends GraphicsProgram{
+
+	//12. გრაფიკული პროგრამის გამოყენებით დახატეთ სახლი.
+	
+	public void run(){
+		add(new GRect(100, 200), 100, 200);
+	}
 
 }
