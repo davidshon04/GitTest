7e8bf2104ab8cb6b8cd3d458b189d4678f0b369b
S <s>
2023-10-10 19:07:32 +0400

    {"types":"C","runs":22}

--- Problem12.java
+++ Problem12.java
@@ -6,12 +6,15 @@ public class Problem12 extends GraphicsProgram{
 	//12. გრაფიკული პროგრამის გამოყენებით დახატეთ სახლი.
 	
 	public void run(){
-		GRect myRect = new GRect(150, 200);
-		add(myRect, 200, 200);
+		GRect myHouse = new GRect(150, 200);
+		add(myHouse, 200, 200);
 		
-		add(new GRect(50, 80), 250, 320);
-		add(new GRect (50, 50), 200, 240);
-		add(new GRect (50, 50), 300, 240);
+		GRect myDoor = new GRect(50, 80);
+		add(myDoor, 250, 320);
+		
+		GRect myWindow = new GRect (50, 50);
+		add(myWindow, 200, 240);
+		add(myWindow, 300, 240);
 //		add(new GLine(), 200, )
 	}
 
