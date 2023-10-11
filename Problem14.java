1d11669c5d2cd74dccf1bc172d6d0df7008d07b5
S <s>
2023-10-11 22:59:10 +0400

    {"types":"C","runs":5}

--- Problem14.java
+++ Problem14.java
@@ -1,4 +1,22 @@
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
-public class Problem14 {
+//	14. დახატეთ რობოტის სახე ფანჯრის ცენტრში.
+
+public class Problem14 extends GraphicsProgram{
+	public void run(){
+		
+//ვიგებ პანელის სიგრძე-სიგანეს:
+		int panelHeight = getHeight();
+		int panelWidth = getWidth();
+		
+//ვიგებ ცენტრის კოორდინატებს:
+		int middleX = panelWidth/2;
+		int middleY = panelHeight/2;
+		
+//ვხატავ რობოტის თავს:
+		GRect robotHead = new GRect (200, 200);
+		add(robotHead, middleX-100, middleY+100);
+	}
 
 }
