506068b6cc66e4d26e1dacecbf435369c3913242
S <s>
2023-10-12 18:17:46 +0400

    {"types":"C","runs":1}

--- SemPr11.java
+++ SemPr11.java
@@ -1,4 +1,17 @@
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
-public class SemPr11 {
+public class SemPr11 extends GraphicsProgram{
+	public void run(){
+		int width = 100;
+		width = width+20;
+		width = 200;
+		
+		GRect rect = new GRect(width, width, width, width);
+		add(rect);
+	}
 
+//	private void testVoid(){
+		
+	}
 }
