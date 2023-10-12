07542b6fa7cd574f0ae6ec114eae5fc48649bff7
S <s>
2023-10-12 19:03:05 +0400

    {"types":"C","runs":9}

--- ConsoleSample.java
+++ ConsoleSample.java
@@ -1,4 +1,10 @@
+import acm.program.ConsoleProgram;
 
-public class ConsoleSample {
+public class ConsoleSample extends ConsoleProgram{
+	public void run(){
+		int kids = readInt();
+		
+		println("Your kids number: " + kids);
+	}
 
 }
