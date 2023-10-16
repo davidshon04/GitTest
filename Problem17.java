5877b36f537d01c6115d4670b2ec0ca08529479e
S <s>
2023-10-16 22:17:43 +0400

    {"types":"C","runs":13}

--- Problem17.java
+++ Problem17.java
@@ -1,4 +1,18 @@
+import acm.program.ConsoleProgram;
 
-public class Problem17 {
+//კონსოლიდან წაიკითხეთ ნამდვილი რიცხვი და დაბეჭდეთ მისი მთელი ნაწილი და მისი 
+//წილადი ნაწილი. მაგალითად მომხმარებელმა თუ შეიყვანა 3.14 თქვენმა პროგრამამ უნდა 
+//დაბეჭდოს 3 და 0.14
+
+public class Problem17 extends ConsoleProgram {
+	public void run() {
+		double realNumber = readDouble ("Enter Real Number: ");
+		
+		int wholeNumber = (int) realNumber;
+		println(wholeNumber);
+		
+		double fraction = realNumber - (double) wholeNumber;
+		println(fraction);
+	}
 
 }
