4fed7de2aaa92dab15d0c62138b3409e1fac97fb
S <s>
2023-10-16 21:33:18 +0400

    {"types":"C","runs":1}

--- Problem15.java
+++ Problem15.java
@@ -1,4 +1,14 @@
+import acm.program.ConsoleProgram;
 
-public class Problem15 {
+//15. კონსოლიდან წაიკითხეთ ორი მთელი რიცხვი და დაბეჭდეთ მათი საშუალო არითმეტიკული.
+
+public class Problem15 extends ConsoleProgram {
+	public void run() {
+		int num1 = readInt("Enter First Number: ");
+		int num2 = readInt("Enter Second Number: ");
+		
+		double average = (num1 + num2)/2;
+		println(average);
+	}
 
 }
