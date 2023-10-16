b8b4686cee4440e5d0d5eff34ea417dcb74142c7
S <s>
2023-10-16 22:40:43 +0400

    {"types":"C","runs":23}

--- Problem18.java
+++ Problem18.java
@@ -1,4 +1,19 @@
+import acm.program.ConsoleProgram;
 
-public class Problem18 {
+//18. კონსოლიდან წაიკითხეთ ორი მთელი რიცხვი და დაბეჭდეთ მათ შორის მაქსიმალური.
 
+public class Problem18 extends ConsoleProgram {
+	public void run() {
+		int num1 = readInt("Enter First Number: ");
+		int num2 = readInt("Enter Second Number: ");
+		
+		if(num1 > num2) {
+			println("Max Number is: " + num1 + ".");
+		} else if(num2 > num1) {
+			println("Max Number is: " + num2 + ".");
+		} else {
+			println("These Numbers are Equal.");
+		}
+	}
+	
 }
