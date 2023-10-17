22f70b98c39ef4fcfcc149840e732b85a9a4f46f
S <s>
2023-10-17 18:36:10 +0400

    {"types":"C","runs":1}

--- SemPr16.java
+++ SemPr16.java
@@ -1,4 +1,18 @@
+import acm.program.ConsoleProgram;
 
-public class SemPr16 {
+//1.კონსოლიდან იკითხეთ რიცხვები, მანამ სანამ შემოსული რიცხვი არ იქნება 0. 
+//0-ის შემოსვლის შემდეგ დაბეჭდეთ შემოსული რიცხვების ჯამი.
+
+public class SemPr16 extends ConsoleProgram {
+
+	//private static final 
+	
+	public void run() {
+		int num = readInt ("Enter the Number: ");
+		while (num != 0) {
+			println (num);
+		}
+		
+	}
 
 }
