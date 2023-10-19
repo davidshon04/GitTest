fdff11988d95156fd6d20b8e4dce267496de36d9
S <s>
2023-10-19 17:33:38 +0400

    {"types":"C","runs":1}

--- Sem8Pr1.java
+++ Sem8Pr1.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
 
-public class Sem8Pr1 {
+// კონსოლიდან იკითხეთ რიცხვები, მანამ სანამ შემოსული რიცხვი არ იქნება 0. 
+// 0-ის შემოსვლის შემდეგ დაბეჭდეთ შემოსული რიცხვების ჯამი.
 
+public class Sem8Pr1 extends ConsoleProgram {
+	
+	private static final int Sentinel = 0;
+	
+	public void run(){
+		int total = 0;
+		
+		while(true) {
+			int num = readInt("Enter Number: ");
+			if(num == Sentinel) break;
+				total += num;
+		}
+		
+		println(total);
+	}
 }
