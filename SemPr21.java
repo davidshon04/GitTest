0a8aa60696c5a50d3c9efcc326bf2c5d89dcd943
S <s>
2023-10-19 17:52:30 +0400

    {"types":"C","runs":5}

--- SemPr21.java
+++ SemPr21.java
@@ -8,8 +8,16 @@ public class SemPr21 extends GraphicsProgram {
 	public void run() {
 		for (int i = 0; i < NUM_LINES; i++) {
 			drawHorizontalLine(i);
-		}
-		
+			drawVerticalLine(i);
+		}	
+	}
+
+	private void drawVerticalLine(int i) {
+		double x = i * ((double) getWidth() / (NUM_LINES - 1));
+		double y1 = 0;
+		double y2 = getHeight();
+		GLine line = new GLine(x, y1, x, y2);
+		add(line);	
 	}
 
 	private void drawHorizontalLine(int i) {
