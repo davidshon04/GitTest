384f635eadda2add002418cbe502d563784c435c
S <s>
2023-10-19 15:56:16 +0400

    {"types":"C","runs":12}

--- Problem22.java
+++ Problem22.java
@@ -1,4 +1,5 @@
 import acm.graphics.GLine;
+import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 //22. ჭადრაკის დაფის დახატვა 8x8 ზე
@@ -7,27 +8,46 @@ public class Problem22 extends GraphicsProgram {
 	
 	private static final int N_Rows = 8;
 	private static final int N_Columns = 8;
+	 
 	
 	public void run(){
-		drawRows();
-		drawColumns();
-	//	colorRects();
+	//	drawRows();
+	//	drawColumns();
+		drawSquares();
+	//	colorSquares();
 	}
 	
-	private void drawColumns() {
-		for(int j = 0; j <= N_Columns; j++){
-			double xCoord = (double) getHeight() / N_Columns;
-			GLine columnLine = new GLine(xCoord * j, 0, xCoord * j, getHeight());
-			add(columnLine);
-		}
+	private void drawSquares() {
+		for(int j = 0; j <= N_Columns; j++) {
+			for(int i = 0; i <= N_Rows; i++){
+				double squareSize = (double) getHeight() / N_Columns;
+				GRect square = new GRect (squareSize, squareSize);
+				double x = j * squareSize;
+				double y = i * squareSize;
+				add(square, x, y);
+			}	
+		}	
 	}
 
-	private void drawRows() {
-		for(int i = 0; i < N_Rows; i++){
-			double yCoord = (double) getHeight() / N_Rows;
-			GLine rowLine = new GLine(0, yCoord * i, getHeight(), yCoord * i);
-			add(rowLine);
-		}
-	}
+//	private void colorSquares() {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	private void drawColumns() {
+//		for(int j = 0; j <= N_Columns; j++){
+//			double xCoord = (double) getHeight() / N_Columns;
+//			GLine columnLine = new GLine(xCoord * j, 0, xCoord * j, getHeight());
+//			add(columnLine);
+//		}
+//	}
+//
+//	private void drawRows() {
+//		for(int i = 0; i <= N_Rows; i++){
+//			double yCoord = (double) getHeight() / N_Rows;
+//			GLine rowLine = new GLine(0, yCoord * i, getHeight(), yCoord * i);
+//			add(rowLine);
+//		}
+//	}
 
 }
