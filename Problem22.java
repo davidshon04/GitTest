c3514d3fa93558337cb861e2ea4d8ba10a0d227f
S <s>
2023-10-19 15:36:12 +0400

    {"types":"C","runs":7}

--- Problem22.java
+++ Problem22.java
@@ -1,4 +1,33 @@
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
 
-public class Problem22 {
+//22. ჭადრაკის დაფის დახატვა 8x8 ზე
+
+public class Problem22 extends GraphicsProgram {
+	
+	private static final int N_Rows = 8;
+	private static final int N_Columns = 8;
+	
+	public void run(){
+		drawRows();
+		drawColumns();
+	//	colorRects();
+	}
+	
+	private void drawColumns() {
+		for(int j = 0; j < N_Columns; j++){
+			int xCoord = getHeight() / N_Columns;
+			GLine columnLine = new GLine(xCoord * j, 0, xCoord * j, getHeight());
+			add(columnLine);
+		}
+	}
+
+	private void drawRows() {
+		for(int i = 0; i < N_Rows; i++){
+			int yCoord = getHeight() / N_Rows;
+			GLine rowLine = new GLine(0, yCoord * i, getWidth(), yCoord * i);
+			add(rowLine);
+		}
+	}
 
 }
