f077eeefe9f2400afc14c22ee69f724e430f605a
S <s>
2023-10-19 16:02:00 +0400

    {"types":"C","runs":14}

--- Problem22att2.java
+++ Problem22att2.java
@@ -1,4 +1,21 @@
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
-public class Problem22att2 {
+public class Problem22att2 extends GraphicsProgram {
+	
+	private static final int N_Rows = 8;
+	private static final int N_Columns = 8;
+	
+	public void run() {
+		for(int i = 0; i <= N_Rows; i++) {
+			for(int j = 0; j <= N_Columns; j++){
+				double squareSize = (double) getHeight() / N_Columns;
+				GRect square = new GRect (squareSize, squareSize);
+				double x = i * squareSize;
+				double y = j * squareSize;
+				add(square, x, y);
+			}	
+		}
+	}
 
 }
