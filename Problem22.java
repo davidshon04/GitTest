77db403ddc1ac22a4de3546e691505dacce93828
S <s>
2023-10-19 15:37:05 +0400

    {"types":"C","runs":8}

--- Problem22.java
+++ Problem22.java
@@ -15,7 +15,7 @@ public class Problem22 extends GraphicsProgram {
 	}
 	
 	private void drawColumns() {
-		for(int j = 0; j < N_Columns; j++){
+		for(int j = 0; j <= N_Columns; j++){
 			int xCoord = getHeight() / N_Columns;
 			GLine columnLine = new GLine(xCoord * j, 0, xCoord * j, getHeight());
 			add(columnLine);
@@ -25,7 +25,7 @@ public class Problem22 extends GraphicsProgram {
 	private void drawRows() {
 		for(int i = 0; i < N_Rows; i++){
 			int yCoord = getHeight() / N_Rows;
-			GLine rowLine = new GLine(0, yCoord * i, getWidth(), yCoord * i);
+			GLine rowLine = new GLine(0, yCoord * i, getHeight(), yCoord * i);
 			add(rowLine);
 		}
 	}
