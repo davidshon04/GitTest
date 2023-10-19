b5c5acbf8f2d6de4cf111c41987722976810a84c
S <s>
2023-10-19 15:30:00 +0400

    {"types":"C","runs":6}

--- Problem21.java
+++ Problem21.java
@@ -6,14 +6,26 @@ import acm.program.GraphicsProgram;
 public class Problem21 extends GraphicsProgram {
 	
 	private static final int N_Rows = 10;
-//	private static final int N_Columns = 10;
+	private static final int N_Columns = 10;
 	
 	public void run() {
+		drawRows();
+		drawColumns();
+	}
+
+	private void drawColumns() {
+		for(int j = 0; j < N_Columns; j++){
+			int xCoord = getWidth() / N_Columns;
+			GLine columnLine = new GLine(xCoord * j, 0, xCoord * j, getHeight());
+			add(columnLine);
+		}
+	}
+
+	private void drawRows() {
 		for(int i = 0; i < N_Rows; i++){
 			int yCoord = getHeight() / N_Rows;
 			GLine rowLine = new GLine(0, yCoord * i, getWidth(), yCoord * i);
 			add(rowLine);
 		}
 	}
-
 }
