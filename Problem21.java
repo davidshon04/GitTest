402726a5bd2000b318faa1b7a939fc3caab6f9fb
S <s>
2023-10-19 15:17:13 +0400

    {"types":"C","runs":2}

--- Problem21.java
+++ Problem21.java
@@ -1,4 +1,19 @@
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
 
-public class Problem21 {
+//21. ხაზებით ბადის დახატვა 10x10 ზე
+
+public class Problem21 extends GraphicsProgram {
+	
+	private static final int N_Rows = 10;
+//	private static final int N_Columns = 10;
+	
+	public void run() {
+	//	int i = getHeight() / N_Rows;
+		for(int i = getHeight() / N_Rows; i < N_Rows; i++){
+			GLine rowLine = new GLine(0, i, getWidth(), i);
+			add(rowLine);
+		}
+	}
 
 }
