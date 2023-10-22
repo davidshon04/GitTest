f81f6b54a106b10ab41dd15ff5e9fbdc99e3a917
S <s>
2023-10-23 01:27:40 +0400

    {"types":"C","runs":1}

--- Problem24.java
+++ Problem24.java
@@ -1,4 +1,17 @@
+import acm.program.ConsoleProgram;
 
-public class Problem24 {
+//24. მომხმარებელს შეყავს მთელი რიცხვი n, პროგრამამ უნდა დაბეჭდოს n ის გამყოფების 
+//რაოდენობა
+
+public class Problem24 extends ConsoleProgram {
+	public void run() {
+//		int n = readInt("Enter the Number: ");
+		
+//		int total = 1;
+//		for(int i = 2; i <= n; i++) {
+//			double div = n / i;
+//			
+//		}
+	}
 
 }
