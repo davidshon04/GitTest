9c0f63ef13107faf20259fd31440e79ae0e4be77
S <s>
2023-10-23 23:17:10 +0400

    {"types":"C","runs":1}

--- Problem24.java
+++ Problem24.java
@@ -5,13 +5,18 @@ import acm.program.ConsoleProgram;
 
 public class Problem24 extends ConsoleProgram {
 	public void run() {
-//		int n = readInt("Enter the Number: ");
+		int n = readInt("Enter the Number: ");
 		
-//		int total = 1;
-//		for(int i = 2; i <= n; i++) {
-//			double div = n / i;
-//			
-//		}
+		int total = 1;
+		for(int i = 2; i <= n; i++) {
+			double div1 = n / i;
+			int div2 = n / i;
+			if(div1 - (double) div2 == 0.0){
+				total += 1;
+			}
+		}
+		
+		println("The Number of Divisors for your Number is: " + total + ".");
 	}
 
 }
