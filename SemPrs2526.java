ed453c01aa79f8727bc5c78d3715c2e1779ca297
S <s>
2023-10-24 17:48:02 +0400

    {"types":"C","runs":4}

--- SemPrs2526.java
+++ SemPrs2526.java
@@ -3,16 +3,31 @@ import acm.program.ConsoleProgram;
 public class SemPrs2526 extends ConsoleProgram {
 	public void run() {
 		int n = readInt("Enter n: ");
+		while(n < 0) {
+			println("n should be >= 0");
+			n = readInt("Enter n: ");
+		}
+		
 		int m = readInt("Enter m: ");
+		while(m < 0) {
+			println("m should be >= 0");
+			m = readInt("Enter m: ");
+		}
+		
 		
 		int gcd = gcd(n, m);
 		println("GCD is: " + gcd);
 		
-	
+		int lcm = lcm(n, m);
+		println("LCM is: " + lcm);
+	}
+
+	private int lcm(int n, int m) {
+		return n * m / gcd(n, m);
 	}
 
 	private int gcd(int n, int m) {
-		for (int i = Math.min(n, m); i >= 1; i--) {
+		for(int i = Math.min(n, m); i >= 1; i--) {
 			if(n % i == 0 && m % i == 0) {
 				return i;
 			}
