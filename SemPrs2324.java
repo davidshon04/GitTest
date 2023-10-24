2493432d3c4457936c7cdeb91e29ff153cd8d845
S <s>
2023-10-24 17:18:35 +0400

    {"types":"C","runs":1}

--- SemPrs2324.java
+++ SemPrs2324.java
@@ -1,4 +1,17 @@
+import acm.program.ConsoleProgram;
 
-public class SemPrs2324 {
+public class SemPrs2324 extends ConsoleProgram {
+	public void run() {
+		int n = readInt("Enter n: ");
+		int sum = calculateSum(n);
+		println("Sum of numbers 1...n is: " + sum + ".");
+	}
 
+	private int calculateSum(int n) {
+		int sum = 0;
+		for(int i = 1; i <= n; i++) {
+			sum += i;  //sum = sum + i;
+		}
+		return sum;
+	}
 }
