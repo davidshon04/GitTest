89f1e6a245f68b2018230dcd3dbf145b73c95f23
S <s>
2023-10-24 17:42:01 +0400

    {"types":"C","runs":3}

--- SemPrs2526.java
+++ SemPrs2526.java
@@ -1,4 +1,22 @@
+import acm.program.ConsoleProgram;
 
-public class SemPrs2526 {
+public class SemPrs2526 extends ConsoleProgram {
+	public void run() {
+		int n = readInt("Enter n: ");
+		int m = readInt("Enter m: ");
+		
+		int gcd = gcd(n, m);
+		println("GCD is: " + gcd);
+		
+	
+	}
 
+	private int gcd(int n, int m) {
+		for (int i = Math.min(n, m); i >= 1; i--) {
+			if(n % i == 0 && m % i == 0) {
+				return i;
+			}
+		}
+		return -1;
+	}
 }
