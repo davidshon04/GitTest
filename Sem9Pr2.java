50c86e0a6a038a60c4729cb5668dc84645ab041f
S <s>
2023-10-24 17:55:54 +0400

    {"types":"C","runs":5}

--- Sem9Pr2.java
+++ Sem9Pr2.java
@@ -1,4 +1,11 @@
+import acm.graphics.GLabel;
+import acm.program.GraphicsProgram;
 
-public class Sem9Pr2 {
+public class Sem9Pr2 extends GraphicsProgram {
+	public void run() {
+		int x = 4;
+		GLabel label = new GLabel("x before method" + x);
+		add(label, 0, 20);
+	}
 
 }
