c16fc4f14b4f042558021e2489c37ab84723e87a
S <s>
2023-10-24 17:29:49 +0400

    {"types":"C","runs":2}

--- SemPrs2324.java
+++ SemPrs2324.java
@@ -1,10 +1,17 @@
 import acm.program.ConsoleProgram;
 
+//23. მომხმარებელს შეყავს მთელი რიცხვი n, პროგრამამ უნდა დაბეჭდოს 1 დან n მდე რიცხვების 
+//ჯამი
+
 public class SemPrs2324 extends ConsoleProgram {
 	public void run() {
 		int n = readInt("Enter n: ");
 		int sum = calculateSum(n);
+		
 		println("Sum of numbers 1...n is: " + sum + ".");
+		
+		int numDivisors =  numberOfDivisors(n);
+		println("Number of Dividors is: " + numDivisors + ".");
 	}
 
 	private int calculateSum(int n) {
@@ -13,5 +20,15 @@ public class SemPrs2324 extends ConsoleProgram {
 			sum += i;  //sum = sum + i;
 		}
 		return sum;
+	}
+	
+	private int numberOfDivisors(int n) {
+		int num = 0;
+		for(int i = 1; i <= n; i++) {
+			if(n % i == 0) {
+				num += 1;
+			}
+		}
+		return num;
 	}
 }
