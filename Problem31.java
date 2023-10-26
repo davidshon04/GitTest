abe972ddd29c29902cbb9060cb8695fc3abf4bba
S <s>
2023-10-26 19:11:10 +0400

    {"types":"C","runs":6}

--- Problem31.java
+++ Problem31.java
@@ -1,4 +1,16 @@
+import acm.program.ConsoleProgram;
 
-public class Problem31 {
+//31. დაწერეთ პროგრამა რომელიც დაადგენს შეყვანილი რიცხვი მარტივია თუ არა 
 
+public class Problem31 extends ConsoleProgram {
+	public void run() {
+		int n = readInt("Enter the Number: ");
+		
+		for(int i = 2; i < n; i++) {
+			double div1 = n / i;
+			int div2 = n / i;
+			if(div1 == (double) div2) break;
+		}
+		
+	}
 }
