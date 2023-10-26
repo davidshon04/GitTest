12ebcc11e8bf4563647d275ba2bd11c253b4a0f1
S <s>
2023-10-26 18:28:59 +0400

    {"types":"C","runs":3}

--- Problem27.java
+++ Problem27.java
@@ -6,11 +6,17 @@ public class Problem27 extends ConsoleProgram {
 	public void run() {
 		int n = readInt("Enter n: ");
 		int m = readInt("Enter m: ");
+		int powered = powerNM(n, m);
 		
+		println("n^m is: " + powered + ".");
+	}
+
+	private int powerNM(int n, int m) {
 		int total = n;
 		for(int i = 1; i < m; i++) {
 			total *= n;
 		}
-		println("n^m is: " + total + ".");
+		return total;
 	}
+	
 }
