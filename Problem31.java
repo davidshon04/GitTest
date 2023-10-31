d730f860d6d5bb0a305d20c940e71f6e8d65e3d1
S <s>
2023-10-31 10:36:57 +0400

    {"types":"C","runs":10}

--- Problem31.java
+++ Problem31.java
@@ -6,16 +6,19 @@ public class Problem31 extends ConsoleProgram {
 	public void run() {
 		int n = readInt("Enter the Number: ");
 		
+		int total = 1;   //because all numbers can be divided by 1.
 		for(int i = 2; i < n; i++) {
-			double div1 = n / i;
-			int div2 = n / i;
-			
-			if(div1 == (double)div2) {
-				println("Not Simple Number");
-			} else {
-				println("Simple Number");
+			double div1 = (double) n / i;    
+			int div2 = (int) (n / i);
+			if(div1 == (double) div2) {
+				total += 1;
 			}
-			 
+			
+			if(total >= 2) {
+				println("Not Simple");
+			} else {
+				println("Simple");
+			}
 		}
 	}
 }
