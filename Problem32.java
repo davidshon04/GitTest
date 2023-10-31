63f241cd22fec2996f9da03a0812aec8d40d4b07
S <s>
2023-10-31 18:00:49 +0400

    {"types":"C","runs":6}

--- Problem32.java
+++ Problem32.java
@@ -1,4 +1,26 @@
+import acm.program.ConsoleProgram;
 
-public class Problem32 {
+// 32. დაწერეთ პროგრამა რომელიც იპოვის და დაბეჭდავს 1000 მდე ყველა მერტივ რიცხვს
+
+public class Problem32 extends ConsoleProgram {
+	
+	private static final int MAX_NUM = 1000;
+	
+	public void run() {
+		for(int i = 0; i < MAX_NUM; i++) {
+			if(isPrime(i)) {
+				println(i);
+			}
+		}
+	}
+
+	private boolean isPrime(int num) {
+		for(int i = 2; i <= Math.sqrt(num); i++) {
+			if(num % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
