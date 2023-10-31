0f41ac8465ae6768af8e460d7e4ed53135a5ac6e
S <s>
2023-10-31 17:32:36 +0400

    {"types":"C","runs":3}

--- Problem29.java
+++ Problem29.java
@@ -5,16 +5,29 @@ import acm.program.ConsoleProgram;
 
 public class Problem29 extends ConsoleProgram {
 	public void run() {
-//		int n = readInt("Enter the Number: ");
-//		int fibonacci = fibonaccimethod(n);
-//	}
-//
-//	private int fibonaccimethod(int n) {
-//		int total = 1;
-//		for(int i = 1; i <= n - 2; i++) {
-//			total += i;
-//		}
-//		return 0;
+		int n = readInt("Enter the Number: ");
+		
+		int fibN = fib(n);
+		println(fibN);
+	}
+
+	
+//	0 1 2 3 4 5 6 7 
+//	0 1 1 2 3 5 8 13
+	private int fib(int n) {
+		if(n == 0 || n == 1) {
+			return n;
+		}
+		int prev2 = 0;
+		int prev1 = 1;
+		
+//		loop
+		for(int i = 2; i <= n; i++) {
+			int current = prev2 + prev1;
+			prev2 = prev1;
+			prev1 = current;
+		}
+		return prev1;
 	}
 
 }
