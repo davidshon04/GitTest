0cf122894d869a04406773eae893a880765243bb
S <s>
2023-10-31 17:15:07 +0400

    {"types":"C","runs":2}

--- SemPr28.java
+++ SemPr28.java
@@ -1,4 +1,25 @@
+import acm.program.ConsoleProgram;
 
-public class SemPr28 {
+//28. მომხმარებელს შეყავს დადებითი რიცხვები, მანამ სანამ არ შეიყვანს -1 ს, დაბეჭდეთ 
+//რაოდენობა რამდენი ლუწი რიცხვი შეყვანა მომხმარებელმა. 
+
+public class SemPr28 extends ConsoleProgram {
+	
+	private static final int SENTINEL = -1;
+	
+	public void run() {
+//		int n = readInt("Enter Number: ");
+		int numEvens = 0;
+		while(true) {
+			int n = readInt("Enter Number: ");
+			if(n == SENTINEL) {
+				break;
+			}
+			if(n % 2 == 0) {
+				numEvens += 1;
+			}
+		}
+		println("Number of Evens is: " + numEvens);
+	}
 
 }
