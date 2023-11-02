676ccf50c548810a57b5fd0c9edee33946cf63a1
S <s>
2023-11-02 17:44:09 +0400

    {"types":"C","runs":5}

--- Problem35.java
+++ Problem35.java
@@ -1,4 +1,34 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class Problem35 {
+//	35. თქვენი ამოცანაა გააკეთოთ მონეტის აგდების სიმულაციები და დათვალოთ საშუალოდ
+//	რამდენჯერ უნდა ავაგდოთ მონეტა რათა ამოვიდეს ბორჯღალო
 
+public class Problem35 extends ConsoleProgram {
+	
+	private static final int NUM_EXPERIMENTS = 10000;
+	
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	
+	public void run() {
+		int sumFlips = 0;
+		for(int i = 0; i < NUM_EXPERIMENTS; i++) {
+			int currFlips = holdExperiment();
+			sumFlips += currFlips;
+		}
+		double avgFlips = sumFlips / NUM_EXPERIMENTS;
+		println(avgFlips);
+	}
+
+	private int holdExperiment() {
+		int numFlips = 0;
+		while(true) {
+			numFlips++;
+			boolean isHeads = rgen.nextBoolean();
+			if(isHeads) {
+				break;
+			}
+		}
+		return numFlips;
+	}
 }
