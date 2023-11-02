d37a84e9f707ad77c06d98d37a6fe47a43ef178b
S <s>
2023-11-02 17:12:17 +0400

    {"types":"C","runs":1}

--- Problem33.java
+++ Problem33.java
@@ -1,4 +1,14 @@
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
-public class Problem33 {
+//	34. კანვასზე დახატეთ 1000 შემთხვევითი რადიუსის მქონე შემთხვევითი ფერით გაფერადებული
+//	წრე, შემთხვევით კოორდინატებზე.
 
+public class Problem33 extends GraphicsProgram {
+	
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	
+	public void run() {
+		println(rgen.nextInt());
+	}
 }
