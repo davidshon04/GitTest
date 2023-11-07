114dc4c41bee2ce741bf01ac4e4a14473f7d4e7f
S <s>
2023-11-08 01:43:28 +0400

    {"types":"C","runs":29}

--- Revision34.java
+++ Revision34.java
@@ -1,4 +1,34 @@
+import java.awt.Color;
 
-public class Revision34 {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
+//34. კანვასზე დახატეთ 1000 შემთხვევითი რადიუსის მქონე შემთხვევითი ფერით გაფერადებული
+//წრე, შემთხვევით კოორდინატებზე.
+
+public class Revision34 extends GraphicsProgram {
+
+	private static final int NUM_CIRCLES = 1000;
+	private static final int MIN_R = 10;
+	private static final int MAX_R = 100;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+
+	public void run() {
+		for (int i = 0; i < NUM_CIRCLES; i++) {
+			addCircle();
+		}
+	}
+
+	private void addCircle() {
+		int radius = rgen.nextInt(MIN_R, MAX_R);
+		double x = rgen.nextDouble(0, getWidth() - 2 * radius);
+		double y = rgen.nextDouble(0, getHeight() - 2 * radius);
+		Color color = rgen.nextColor();
+
+		GOval circle = new GOval(2 * radius, 2 * radius);
+		circle.setFilled(true);
+		circle.setColor(color);
+		add(circle, x, y);
+	}
 }
