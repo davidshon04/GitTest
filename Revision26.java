723206c7c984079284a355e34dbda918263aa83b
S <s>
2023-11-08 00:36:32 +0400

    {"types":"C","runs":5}

--- Revision26.java
+++ Revision26.java
@@ -8,11 +8,13 @@ public class Revision26 extends ConsoleProgram {
 		int a = readInt("Enter a: ");
 		while (a <= 0) {
 			println("a should be positive!");
+			a = readInt("Enter a: ");
 		}
 
 		int b = readInt("Enter b: ");
 		while (b <= 0) {
 			println("b should be positive!");
+			b = readInt("Enter b: ");
 		}
 
 		int gcd = gcdCounter(a, b);
