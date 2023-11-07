fe8db208d5e00be7a403bd4a6b8d93da5685bc28
S <s>
2023-11-08 01:18:46 +0400

    {"types":"C","runs":22}

--- Revision31.java
+++ Revision31.java
@@ -1,4 +1,24 @@
+import acm.program.ConsoleProgram;
 
-public class Revision31 {
+//31. დაწერეთ პროგრამა რომელიც დაადგენს შეყვანილი რიცხვი მარტივია თუ არა
 
+public class Revision31 extends ConsoleProgram {
+	public void run() {
+		int n = readInt("Enter number: ");
+		boolean isSimple = testSimple(n);
+		if(isSimple) {
+			println("Simple");
+		} else {
+			println("not Simple");
+		}
+	}
+
+	private boolean testSimple(int n) {
+		for (int i = 2; i < n; i++) {
+			if(n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
