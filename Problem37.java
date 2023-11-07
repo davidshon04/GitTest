961878cf512b476a86f6dca10cdeef1702bdbf09
S <s>
2023-11-07 17:12:51 +0400

    {"types":"C","runs":1}

--- Problem37.java
+++ Problem37.java
@@ -1,4 +1,36 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class Problem37 {
+//	37. რულეტკა, მომხმარებელს შეყავს 0 დან 36 მდე რიცხვი, შემდეგ ხდება რულეტკის
+//	დატრიალების სიმულაცია, კონსოლში უნდა დაიბეჭდოს ამოსული რიცხვი და მოიგო თუ არა
+//	დადებულმა რიცხვმა
 
+public class Problem37 extends ConsoleProgram {
+	
+	private static final int MIN_VALUE = 0;
+	private static final int MAX_VALUE = 36;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	
+	public void run() {
+		int betNumber = readBet();
+		int rouletteNumber = spinRouletee();
+		
+		if(betNumber == rouletteNumber) {
+			println("You won!");
+		} else {
+			println("You lost!");
+		}
+	}
+
+	private int spinRouletee() {
+		int rouletteResult = rgen.nextInt(MIN_VALUE, MAX_VALUE);
+		println("Roulette result is: " + rouletteResult);
+		
+		return rouletteResult;
+	}
+
+	private int readBet() {
+		int bet = readInt("Enter bet number(0 - 36)");
+		return bet;
+	}
 }
