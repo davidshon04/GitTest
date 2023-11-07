82919a6b9143e508693b546f702a91276585552e
S <s>
2023-11-08 01:22:31 +0400

    {"types":"C","runs":24}

--- Revision31.java
+++ Revision31.java
@@ -4,19 +4,21 @@ import acm.program.ConsoleProgram;
 
 public class Revision31 extends ConsoleProgram {
 	public void run() {
-		while(true){
 		int n = readInt("Enter number: ");
 		boolean isSimple = testSimple(n);
-		if(isSimple) {
+		if (isSimple) {
 			println("Simple");
 		} else {
 			println("not Simple");
 		}
 	}
-	}
+
 	private boolean testSimple(int n) {
+		if (n == 1) {
+			return false;
+		}
 		for (int i = 2; i < n; i++) {
-			if(n % i == 0) {
+			if (n % i == 0) {
 				return false;
 			}
 		}
