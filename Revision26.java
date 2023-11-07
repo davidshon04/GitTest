1b3e598134e5d1eb81da209c7375c37064cddccf
S <s>
2023-11-08 00:30:15 +0400

    {"types":"C","runs":1}

--- Revision26.java
+++ Revision26.java
@@ -1,4 +1,23 @@
+import acm.program.ConsoleProgram;
 
-public class Revision26 {
+//26. მომხმარებელს შეჰყავს ორი რიცხვი თქვენმა პროგრამამ უნდა დაბეჭდოს ამ ორი რიცხვის
+//უდიდესი საერთო გამყოფი
 
+public class Revision26 extends ConsoleProgram {
+	public void run() {
+		int a = readInt("Enter a: ");
+		int b = readInt("Enter b: ");
+		int gcd = gcdCounter(a, b);
+		println("GCD of the entered numbers is: " + gcd);
+	}
+
+	private int gcdCounter(int a, int b) {
+		int maybeGCD = Math.min(a, b);
+		for(int i = maybeGCD; i > 0; i--) {
+			if(a % i == 0 && b % i == 0) {
+				maybeGCD = i;
+			}
+		}
+		return maybeGCD;
+	}
 }
