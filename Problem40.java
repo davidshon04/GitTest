cd54040bf59f841855f0314e9def1c672c96b966
S <s>
2023-11-07 18:01:20 +0400

    {"types":"C","runs":13}

--- Problem40.java
+++ Problem40.java
@@ -1,4 +1,44 @@
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
-public class Problem40 {
+//40. წინა ამოცანის გაგრძელება, დაამატეთ ვერტიკალური კედლებიდან ასხლეტვა. ანუ ბურთი
+//მოძრაობს მარცხენა კედლიდან მარჯვნივ. როგორც კი მარჯვენა კედელს დაეტაკება იმავე
+//სიჩქარით უნდა გააგრძელოს მოძრაობა მაჯვენა კედლიდან მარცხნივ. როგორც კი მარცხენა
+//კედელს დაეტაკება უნდა გააგრძელოს მარჯვნივ და ა.შ. უსასრულოდ.
 
+public class Problem40 extends GraphicsProgram {
+	private static final int PAUSE_ME = 100;
+	private static final int INITIAL_VELOCITY = 5;
+	private static final int RADIUS = 50;
+	
+	public void run() {
+		GOval circle = createInitialCircle();
+		int vx = INITIAL_VELOCITY;
+		int vy = 0;
+		while(circle.getX() <= getWidth() - 2 * RADIUS) {
+			circle.move(vx, vy);
+			pause(PAUSE_ME);
+		}
+	}
+
+	private GOval createInitialCircle() {
+		GOval circle = new GOval(2 * RADIUS, 2 * RADIUS);
+		circle.setFilled(true);
+		int x = getWidth() / 2 - RADIUS;
+		int y = getHeight() / 2 - RADIUS;
+		add(circle, x, y);
+		return circle;
+	}
 }
+//int initialX = getWidth() / 2 - RADIUS;
+//int initialY = getHeight() / 2 - RADIUS;
+//GOval ball = new GOval(2 * RADIUS, 2 * RADIUS);
+//add(ball, initialX, initialY);
+//ball.setFilled(true);
+//
+//int finalX = getWidth() - 2 * RADIUS;
+//while (ball.getX() < finalX) {
+//	ball.move(1, 0);
+//	pause(5);
+//}
+//}
