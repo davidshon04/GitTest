6df24bc6c4c1ad0859a5e47b32a8a13565a1cb91
S <s>
2023-11-08 01:27:20 +0400

    {"types":"C","runs":25}

--- Revision32.java
+++ Revision32.java
@@ -1,4 +1,29 @@
+import acm.program.ConsoleProgram;
 
-public class Revision32 {
+//32. დაწერეთ პროგრამა რომელიც იპოვის და დაბეჭდავს 10000-მდე ყველა მერტივ რიცხვს
 
+public class Revision32 extends ConsoleProgram {
+
+	private static final int MAX_NUM = 10000;
+
+	public void run() {
+		for (int i = 1; i <= MAX_NUM; i++) {
+			boolean isSimple = testSimple(i);
+			if (isSimple) {
+				println(i);
+			}
+		}
+	}
+
+	private boolean testSimple(int n) {
+		if (n == 1) {
+			return false;
+		}
+		for (int i = 2; i < n; i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
