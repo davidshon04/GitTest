90972bed0f81db6f4893105d0321ecbccb57f581
S <s>
2023-11-08 13:04:58 +0400

    {"types":"C","runs":0}

--- Revision35.java
+++ Revision35.java
@@ -1,4 +1,18 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class Revision35 {
+//35. თქვენი ამოცანაა გააკეთოთ მონეტის აგდების სიმულაციები და დათვალოთ საშუალოდ
+//რამდენჯერ უნდა ავაგდოთ მონეტა რათა ამოვიდეს ბორჯღალო
 
+public class Revision35 extends ConsoleProgram {
+	
+	private static final int NUMBER_OF_TRIALS = 10000;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	
+	public void run() {
+//		int x = 3;
+//		for (int i = 0; i < NUMBER_OF_TRIALS; i++) {
+//			
+//		}
+	}
 }
