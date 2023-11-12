8c93772dab8d7a7ab463c04f30f068b94ae40887
S <s>
2023-11-13 01:19:03 +0400

    {"types":"C","runs":20}

--- Revision37.java
+++ Revision37.java
@@ -1,4 +1,14 @@
+import acm.program.ConsoleProgram;
 
-public class Revision37 {
+//37. რულეტკა, მომხმარებელს შეყავს 0 დან 36 მდე რიცხვი, შემდეგ ხდება რულეტკის
+//დატრიალების სიმულაცია, კონსოლში უნდა დაიბეჭდოს ამოსული რიცხვი და მოიგო თუ არა
+//დადებულმა რიცხვმა
 
+public class Revision37 extends ConsoleProgram {
+	public void run() {
+		int n = readInt("Enter Number between 0 and 36: ");
+		while(n < 0 && n > 36) {
+			n = readInt("Enter Number between 0 and 36: ");
+		}
+	}
 }
