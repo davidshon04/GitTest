f237c09a846ce4c4e02fb14422531a7cd550f92d
S <s>
2023-11-13 01:47:32 +0400

    {"types":"C","runs":23}

--- Revision37.java
+++ Revision37.java
@@ -6,26 +6,25 @@ import acm.util.RandomGenerator;
 //დადებულმა რიცხვმა
 
 public class Revision37 extends ConsoleProgram {
-	
+
 	RandomGenerator rgen = RandomGenerator.getInstance();
-	
+
 	public void run() {
-		while(true){
 		int n = readInt("Enter Number between 0 and 36: ");
-		while(n < 0 || n > 36) {
+		while (n < 0 || n > 36) {
 			n = readInt("Enter Number between 0 and 36: ");
 		}
-		
+
 		int roulette = rouletteSimulation();
 		println("The winning number is: " + roulette);
-		
+
 		if (n == roulette) {
 			println("Winner!");
 		} else {
 			println("Loser!");
 		}
 	}
-	}
+
 	private int rouletteSimulation() {
 		int rouletteNumber = rgen.nextInt(0, 36);
 		return rouletteNumber;
