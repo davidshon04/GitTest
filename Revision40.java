575dc018aa7679ea6ff07d2e51085b1c1e8717e5
S <s>
2023-11-13 17:37:13 +0400

    {"types":"C","runs":4}

--- Revision40.java
+++ Revision40.java
@@ -1,4 +1,29 @@
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
-public class Revision40 {
+//40. წინა ამოცანის გაგრძელება, დაამატეთ ვერტიკალური კედლებიდან ასხლეტვა. ანუ ბურთი
+//მოძრაობს მარცხენა კედლიდან მარჯვნივ. როგორც კი მარჯვენა კედელს დაეტაკება იმავე
+//სიჩქარით უნდა გააგრძელოს მოძრაობა მაჯვენა კედლიდან მარცხნივ. როგორც კი მარცხენა
+//კედელს დაეტაკება უნდა გააგრძელოს მარჯვნივ და ა.შ. უსასრულოდ.
 
+public class Revision40 extends GraphicsProgram {
+	
+	private static final int DIAMETER = 50;
+	private static final int VELOCITY = 3;
+	private static final int PAUSE_TIME = 5;
+	
+	public void run() {
+		GOval circle = new GOval(DIAMETER, DIAMETER);
+		add(circle, 0, getHeight() / 2 - DIAMETER / 2);
+		circle.setFilled(true);
+
+		int vx = VELOCITY;
+		while (true) {
+			if(circle.getX() + DIAMETER > getWidth() || circle.getX() < 0) {
+				vx = -vx;
+			}
+			circle.move(vx, 0);
+			pause(PAUSE_TIME);
+		}
+	}
 }
