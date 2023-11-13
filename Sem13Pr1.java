2dece5cbd0a668fffba85db76c2c042604987678
S <s>
2023-11-13 17:52:09 +0400

    {"types":"C","runs":8}

--- Sem13Pr1.java
+++ Sem13Pr1.java
@@ -8,5 +8,10 @@ public class Sem13Pr1 extends GraphicsProgram {
 	public void run() {
 		GLine line = new GLine(0, getHeight() / 2, getWidth(), getHeight() / 2);
 		add(line);
+		
+		while(line.getY() < getHeight() / 2) {
+			line.move(0, 1);
+			pause(5);
+		}
 	}
 }
