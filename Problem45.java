4536efd14d4258f42893961cd1b54be1b03a6cc7
S <s>
2023-11-16 17:58:19 +0400

    {"types":"C","runs":2}

--- Problem45.java
+++ Problem45.java
@@ -4,20 +4,20 @@ import acm.program.ConsoleProgram;
 //რამდენჯერ გვხვდება მოცემულ ტექსტში მოცემული სიმბოლო 
 
 public class Problem45 extends ConsoleProgram {
-	
+
 	public void run() {
-		String str = readLine("Enter str: ");
-		String ch = readLine("Enter char: ");
+		String str = readLine("Enter text: ");
+		String ch = readLine("Enter symbol: ");
 		char firstChar = ch.charAt(0);
-	
+
 		int n = 0;
 		int length = str.length();
 		for (int i = 0; i < length; i++) {
-			if(firstChar == str.charAt(i)) {
+			if (firstChar == str.charAt(i)) {
 				n++;
 			}
 		}
-		
+
 		println(n);
 	}
 }
