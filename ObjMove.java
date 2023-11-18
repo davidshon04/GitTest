a9e78beec166c88fb5434b7e859d7a648e12b19c
S <s>
2023-11-19 03:43:17 +0400

    {"types":"C","runs":46}

--- ObjMove.java
+++ ObjMove.java
@@ -18,7 +18,7 @@ public class ObjMove extends GraphicsProgram{
 	
 	public void mouseMoved(MouseEvent e) {
 		GObject obj = getElementAt(e.getX(), e.getY());
-		prevX = e.getX();
+		
 		obj = rect;
 		obj.move(e.getX() - prevX, 0);
 		
