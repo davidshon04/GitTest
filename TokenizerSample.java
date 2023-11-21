4549cb5ea3b787d9ba644ce0b4c180b66a4e8482
S <s>
2023-11-21 17:59:27 +0400

    {"types":"C","runs":12}

--- TokenizerSample.java
+++ TokenizerSample.java
@@ -2,12 +2,14 @@ import java.util.StringTokenizer;
 
 import acm.program.ConsoleProgram;
 
-public class TokenizerSample extends ConsoleProgram{
+public class TokenizerSample extends ConsoleProgram {
 	public void run() {
-		String text = "markoza magaria";
-		StringTokenizer tokenizer = new StringTokenizer(text);
-		
-		while(tokenizer.hasMoreTokens()){
+		String text = "markoza magaria, shermadini bandzia. basri gelitaevi";
+		// StringTokenizer tokenizer = new StringTokenizer(text);
+
+		StringTokenizer tokenizer = new StringTokenizer(text, " .,");
+
+		while (tokenizer.hasMoreTokens()) {
 			String currToken = tokenizer.nextToken();
 			println(currToken);
 		}
