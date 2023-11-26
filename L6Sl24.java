9398e5fffb323f84daa3dc6dd52afca157d5cba4
S <s>
2023-11-26 18:34:02 +0400

    {"types":"C","runs":25}

--- L6Sl24.java
+++ L6Sl24.java
@@ -1,4 +1,13 @@
+import acm.program.ConsoleProgram;
 
-public class L6Sl24 {
+public class L6Sl24 extends ConsoleProgram {
+	public void run() {
+		boolean lookingForLove = true;
+		int x = 35;
+		if (lookingForLove) {
+			int y = 43;
+			println(x + y);
+		}
+	}
 
 }
