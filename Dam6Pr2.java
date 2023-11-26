90a896f820751d9e78572bc6aaebdc6647c9a58e
S <s>
2023-11-26 19:15:02 +0400

    {"types":"C","runs":10}

--- Dam6Pr2.java
+++ Dam6Pr2.java
@@ -1,4 +1,23 @@
+import acm.program.ConsoleProgram;
 
-public class Dam6Pr2 {
+//კონსოლში შემოდის ორი რიცხვი. რიცხვების შემდეგ შემოდის ერთი სიმბოლო: “+”,” -”, “*” ან “/” . 
+//იმის მიხედვით თუ რომელი სიმბოლო შემოვიდა, დაბეჭდეთ შესაბამისი ოპერაციის შედეგი
 
+public class Dam6Pr2 extends ConsoleProgram{
+	public void run() {
+		int n1 = readInt("n1: ");
+		int n2 = readInt("n2: ");
+		String symbol = readLine("Enter symbol(+, -, *, /): ");
+		char symb = symbol.charAt(0);
+		
+		if (symb == '+') {
+			println(n1 + n2);
+		} else if (symb == '-') {
+			println(n1 - n2);
+		} else if (symb == '*') {
+			println(n1 * n2);
+		} else if (symb == '/') {
+			println(n1 / n2);
+		}
+	}
 }
