cff7801df0292390180cd4663a0d59f6a86dbf10
S <s>
2023-11-26 17:21:04 +0400

    {"types":"C","runs":12}

--- Dam5Pr1.java
+++ Dam5Pr1.java
@@ -1,10 +1,7 @@
-import java.awt.Font;
-
 import acm.graphics.GLabel;
 import acm.program.GraphicsProgram;
 
 //GraphicsProgram - ფანჯრის ცენტრში გამოიტანეთ წარწერა: "width: x, height: y"  სადაც x არის ფანჯრის სიგანე, y - სიმაღლე. 
-//წარწერის ფონტი უნდა იყოს "London" , ზომა - 42 ლ, ფერი - წითელი.
 
 public class Dam5Pr1 extends GraphicsProgram {
 	public void run() {
@@ -12,7 +9,6 @@ public class Dam5Pr1 extends GraphicsProgram {
 		double y = getHeight();
 		String text = "width: " + x + ", height: " + y;
 		GLabel str = new GLabel(text);
-		str.setFont("London");
 		add(str, getWidth() / 2 - str.getWidth() / 2, getHeight() / 2 - str.getAscent() / 2);
 	}
 }
