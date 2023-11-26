2a7cb383be71c0e3be86e0c6708b6dd086ea1837
S <s>
2023-11-26 18:09:08 +0400

    {"types":"C","runs":15}

--- Dam5Pr2.java
+++ Dam5Pr2.java
@@ -11,12 +11,26 @@ public class Dam5Pr2 extends GraphicsProgram {
 	private static final int HOUSE_HEIGHT = 200;
 	private static final int DOOR_WIDTH = 50;
 	private static final int DOOR_HEIGHT = 70;
+	private static final int WINDOW_WIDTH = 50;
+	private static final int WINDOW_HEIGHT = 70;
+
 
 	public void run() {
 		drawHouse();
 		drawDoor();
-
+		
+		GRect window = new GRect(WINDOW_WIDTH, WINDOW_HEIGHT);
+		window.setFilled(true);
+		window.setColor(Color.RED);
+		double x = (getWidth() - HOUSE_WIDTH) / 2;
+		double y = (getHeight() - HOUSE_HEIGHT) / 2 + 20;
+		add(window, x, y);
+		
+		window = new GRect(WINDOW_WIDTH, WINDOW_HEIGHT);
+		x = (getWidth() - HOUSE_WIDTH) / 2 + HOUSE_WIDTH - WINDOW_WIDTH;
+		add(window, x, y);
 	}
+
 
 	private GRect drawDoor() {
 		GRect door = new GRect(DOOR_WIDTH, DOOR_HEIGHT);
