88b91e0faf0b24676edf469a75ef543ce21f8b8a
S <s>
2023-11-26 18:46:25 +0400

    {"types":"C","runs":1}

--- L6Sl85.java
+++ L6Sl85.java
@@ -1,4 +1,12 @@
+import acm.program.ConsoleProgram;
 
-public class L6Sl85 {
+public class L6Sl85 extends ConsoleProgram {
 
+	private static final int NUM_LINES = 100;
+	
+	public void run() {
+		for (int i = 0; i < NUM_LINES; i++) {
+			println("David is Cool!");
+		}
+	}
 }
