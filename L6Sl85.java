6df05ad0a7db219f2bc0ae825c09103f5b462779
S <s>
2023-11-26 18:49:24 +0400

    {"types":"C","runs":5}

--- L6Sl85.java
+++ L6Sl85.java
@@ -5,14 +5,14 @@ public class L6Sl85 extends ConsoleProgram {
 	private static final int NUM_LINES = 20;
 
 	public void run() {
-		// for (int i = 0; i < NUM_LINES; i++) {
-		// println("David is Cool! " + i);
-		// }
+		 for (int i = 0; i < NUM_LINES; i++) {
+			 println("David is Cool! " + i);
+		 }
 
-		int num = 0;
-		while (num < NUM_LINES) {
-			println("David is Cool! " + num);
-			num++;
-		}
+//		int num = 0;
+//		while (num < NUM_LINES) {
+//			println("David is Cool! " + num);
+//			num++;
+//		}
 	}
 }
