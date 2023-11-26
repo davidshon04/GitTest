ef2faa7c1053f972c8d7c03a04046dc3d301b464
S <s>
2023-11-26 18:27:00 +0400

    {"types":"C","runs":20}

--- L6SL11.java
+++ L6SL11.java
@@ -1,4 +1,12 @@
+import acm.program.ConsoleProgram;
 
-public class L6SL11 {
-
+public class L6SL11 extends ConsoleProgram {
+	public void run() {
+		int n = readInt("Enter number: ");
+		if (n == 0) {
+			println("The number is zero");
+		} else {
+			println("The number isn't zero");
+		}
+	}
 }
