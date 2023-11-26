37f2336619c95e7af0fe2400d78eb6ac0167c677
S <s>
2023-11-26 21:06:18 +0400

    {"types":"C","runs":34}

--- L7Sl161.java
+++ L7Sl161.java
@@ -1,4 +1,24 @@
+import acm.program.ConsoleProgram;
 
-public class L7Sl161 {
+// write numbers which are squares
 
+public class L7Sl161 extends ConsoleProgram {
+
+	private static final int MAX_NUM = 300;
+	
+	public void run() {
+		for (int i = 1; i <= MAX_NUM; i++) {
+			if (isSquare(i)) {
+				println(i + ", square of " + Math.sqrt(i));
+			}
+		}
+	}
+
+	private boolean isSquare(int i) {
+		double root = Math.sqrt((double) i);
+		if (root == Math.floor(root)) {
+			return true;
+		}
+		return false;
+	}
 }
