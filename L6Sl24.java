24ccf4cc46f8acb0e8b7790b6e277b1f52dd6293
S <s>
2023-11-26 18:37:15 +0400

    {"types":"C","runs":26}

--- L6Sl24.java
+++ L6Sl24.java
@@ -2,11 +2,13 @@ import acm.program.ConsoleProgram;
 
 public class L6Sl24 extends ConsoleProgram {
 	public void run() {
-		boolean lookingForLove = true;
+		boolean lookingForLove = readBoolean("Looking for love?", "y", "n");
 		int x = 35;
 		if (lookingForLove) {
 			int y = 43;
 			println(x + y);
+		} else {
+			println(x);
 		}
 	}
 
