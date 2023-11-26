3ac02935c571418fb274ebc6660f33e2fe974221
S <s>
2023-11-26 16:29:28 +0400

    {"types":"C","runs":3}

--- L5Sl10.java
+++ L5Sl10.java
@@ -2,10 +2,10 @@ import acm.program.ConsoleProgram;
 
 public class L5Sl10 extends ConsoleProgram {
 	public void run() {
-		println("This program adds two Integers");
+		println("This program adds two Integers.");
 		int n1 = readInt("Enter first Integer: ");
 		int n2 = readInt("Enter second Integer: ");
 		int sum = n1 + n2;
-		println("Sum of the Integers is: " + sum);
+		println("Sum of the Integers is: " + sum + ".");
 	}
 }
