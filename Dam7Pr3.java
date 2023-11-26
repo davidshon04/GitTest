feebe1714ecced34355ff2bf8ee86c2d925c51e2
S <s>
2023-11-26 21:16:08 +0400

    {"types":"C","runs":38}

--- Dam7Pr3.java
+++ Dam7Pr3.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
 
-public class Dam7Pr3 {
+//	კონსოლიდან შემოდის დადებითი მთელი რიცხვი. დაბეჭდეთ მისი ყველა გამყოფი.
 
+public class Dam7Pr3 extends ConsoleProgram {
+	public void run() {
+		int n = readInt("Enter number: ");
+		while (n <= 0) {
+			n = readInt("Enter number: ");
+		}
+		
+		int numDiv = 0;
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0) {
+				println(i);
+				numDiv++;
+			}
+		}
+		println("The number has " + numDiv + "Divisors.");
+	}
 }
