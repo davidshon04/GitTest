134452530316761c2f3ce2d840213b198f9adbfa
S <s>
2023-11-26 20:00:33 +0400

    {"types":"C","runs":21}

--- L7Sl66.java
+++ L7Sl66.java
@@ -1,4 +1,15 @@
+import acm.program.ConsoleProgram;
 
-public class L7Sl66 {
+//Meters to cm method
 
+public class L7Sl66 extends ConsoleProgram {
+	public void run() {
+		double cms = metersToCms(5.7);
+		println(cms);
+	}
+
+	private double metersToCms(double d) {
+		double cms = d * 100;
+		return cms;
+	}
 }
