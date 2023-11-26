469a99fed29f8bf4a61c2f8f7908cdeaf9403ed0
S <s>
2023-11-26 19:03:15 +0400

    {"types":"C","runs":8}

--- Dam6Pr1.java
+++ Dam6Pr1.java
@@ -1,4 +1,20 @@
+import acm.program.ConsoleProgram;
 
-public class Dam6Pr1 {
+//	კონსოლიდან იკითხეთ რიცხვები, მანამ სანამ შემოსული რიცხვი არ იქნება 0. 0-ის შემოსვლის შემდეგ დაბეჭდეთ შემოსული რიცხვების ჯამი
 
+public class Dam6Pr1 extends ConsoleProgram {
+
+	private static final int SENTINEL = 0;
+
+	public void run() {
+		int sum = 0;
+		while (true) {
+			int n = readInt("Enter number: ");
+			if (n == SENTINEL) {
+				break;
+			}
+			sum += n;
+		}
+		println("Sum is: " + sum);
+	}
 }
