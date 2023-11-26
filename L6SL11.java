9ef40f184f1598786fe57ba38b26a534bd0b4ac6
S <s>
2023-11-26 18:28:39 +0400

    {"types":"C","runs":22}

--- L6SL11.java
+++ L6SL11.java
@@ -5,8 +5,12 @@ public class L6SL11 extends ConsoleProgram {
 		int n = readInt("Enter number: ");
 		if (n == 0) {
 			println("The number is zero");
+		} else if (n > 0){
+			println("The number isn't zero");
+			println("The number is positive Integer");
 		} else {
 			println("The number isn't zero");
+			println("The number is negative Integer");
 		}
 	}
 }
