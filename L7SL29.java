cddd1182bf1f9c26c94207a9e9492da90a256e21
S <s>
2023-11-26 19:53:40 +0400

    {"types":"C","runs":20}

--- L7SL29.java
+++ L7SL29.java
@@ -1,4 +1,16 @@
+import acm.program.ConsoleProgram;
 
-public class L7SL29 {
+// create a method to calculate two double's average
 
+public class L7SL29 extends ConsoleProgram {
+	public void run() {
+		double average = calculateAverage(5.8, 67.4);
+		println("Average is: " + average);
+	}
+
+	private double calculateAverage(double d, double e) {
+		double sum = d + e;
+		double average = sum / 2;
+		return average;
+	}
 }
