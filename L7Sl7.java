482d21e9e18d0a098deb6abb929004e5b98c0979
S <s>
2023-11-26 19:47:02 +0400

    {"types":"C","runs":16}

--- L7Sl7.java
+++ L7Sl7.java
@@ -1,4 +1,26 @@
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
-public class L7Sl7 {
+// draw Checkerboard
+
+public class L7Sl7 extends GraphicsProgram {
+
+	private static final int N_ROWS = 8;
+	private static final int N_COLUMNS = 8;
+
+	public void run() {
+		for (int i = 0; i < N_ROWS; i++) {
+			for (int j = 0; j < N_COLUMNS; j++) {
+				double squareWidth = getHeight() / N_ROWS;
+				GRect square = new GRect(squareWidth, squareWidth);
+				double x = squareWidth * j;
+				double y = squareWidth * i;
+				if ((i + j) % 2 != 0) {
+					square.setFilled(true);
+				}
+				add(square, x, y);
+			}
+		}
+	}
 
 }
