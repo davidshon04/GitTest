11db2fb82681460a6ea46621ae84c747dd7fcdb8
S <s>
2023-11-26 20:41:41 +0400

    {"types":"C","runs":32}

--- L7Sl112.java
+++ L7Sl112.java
@@ -6,8 +6,7 @@ public class L7Sl112 extends ConsoleProgram {
 	public void run() {
 		int n = readInt("Enter number: ");
 		for (int i = 0; i <= n; i++) {
-			int factN = calculateFactorial(i);
-			println(i + "! = " + factN);
+			println(i + "! = " + calculateFactorial(i));
 		}
 	}
 
