85bb185cfbf20bf7f32788b84b9b2664346c82a3
S <s>
2023-11-26 18:55:18 +0400

    {"types":"C","runs":6}

--- L6Sl24.java
+++ L6Sl24.java
@@ -1,5 +1,7 @@
 import acm.program.ConsoleProgram;
 
+//variable love story
+
 public class L6Sl24 extends ConsoleProgram {
 	public void run() {
 		boolean lookingForLove = readBoolean("Looking for love?");
