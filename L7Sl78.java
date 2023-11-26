319ebcb071973d59dfbc0de52dfe20bf38b18dd6
S <s>
2023-11-26 20:13:09 +0400

    {"types":"C","runs":24}

--- L7Sl78.java
+++ L7Sl78.java
@@ -1,4 +1,20 @@
+import acm.program.ConsoleProgram;
 
-public class L7Sl78 {
+// you get two numbers, return larger
 
+public class L7Sl78 extends ConsoleProgram {
+	public void run() {
+		int n1 = readInt("n1: ");
+		int n2 = readInt("n2: ");
+		int larger = returnLarger(n1, n2);
+		println(larger);
+	}
+
+	private int returnLarger(int n1, int n2) {
+		if (n1 >= n2) {
+			return n1;
+		} else {
+			return n2;
+		}
+	}
 }
