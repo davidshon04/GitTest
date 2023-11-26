6ed9698ba2b5e4b96f4e760a115bf8df58a1f652
S <s>
2023-11-26 17:56:29 +0400

    {"types":"C","runs":13}

--- Dam5Pr2.java
+++ Dam5Pr2.java
@@ -1,4 +1,28 @@
+import java.awt.Color;
 
-public class Dam5Pr2 {
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
+
+//დახატეთ ფერადი სახლი ფანჯრის ცენტრში
+
+public class Dam5Pr2 extends GraphicsProgram {
+
+	private static final int HOUSE_WIDTH = 100;
+	private static final int HOUSE_HEIGHT = 200;
+
+	public void run() {
+		drawHouse();
+
+	}
+
+	private GRect drawHouse() {
+		GRect house = new GRect(HOUSE_WIDTH, HOUSE_HEIGHT);
+		house.setFilled(true);
+		house.setColor(Color.YELLOW);
+		double x = (getWidth() - HOUSE_WIDTH) / 2;
+		double y = (getHeight() - HOUSE_HEIGHT) / 2;
+		add(house, x, y);
+		return house;
+	}
 
 }
