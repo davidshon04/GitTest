79f95398ada8c73a886c9c379dac955dd6a73136
S <s>
2023-11-27 23:31:44 +0400

    {"types":"C","runs":3}

--- L8Sl5.java
+++ L8Sl5.java
@@ -4,17 +4,22 @@ import acm.program.ConsoleProgram;
 
 public class L8Sl5 extends ConsoleProgram {
 	public void run() {
-		int n = readInt("Enter number of objects in the set: ");
-		int k = readInt("Enter number to be chosen: ");
-		int combin = calculateFactorial(n) / (calculateFactorial(k) * calculateFactorial(n - k));
-		println(combin);
+//		int n = readInt("Enter number of objects in the set: ");
+//		int k = readInt("Enter number to be chosen: ");
+//		int combin = calculateFactorial(n) / (calculateFactorial(k) * calculateFactorial(n - k));
+//		println(combin);
+		int num = 5;
+		cow(num);
 	}
-	
-	private int calculateFactorial(int number) {
-		int factorial = 1;
-		for (int i = 1; i <= number; i++) {
-			factorial *= i;
-		}
-		return factorial;
+//	
+	private void cow(int grass){
+		println(grass);
 	}
+//	private int calculateFactorial(int number) {
+//		int factorial = 1;
+//		for (int i = 1; i <= number; i++) {
+//			factorial *= i;
+//		}
+//		return factorial;
+//	}
 }
