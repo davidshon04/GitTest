8ad694dcd78d9694ec0a9c6020da1978538bcfd0
S <s>
2023-11-27 23:23:29 +0400

    {"types":"C","runs":1}

--- L8Sl5.java
+++ L8Sl5.java
@@ -1,4 +1,20 @@
+import acm.program.ConsoleProgram;
 
-public class L8Sl5 {
+// Create a method to calculate combinations
 
+public class L8Sl5 extends ConsoleProgram {
+	public void run() {
+		int n = readInt("Enter number of objects in the set: ");
+		int k = readInt("Enter number to be chosen: ");
+		double combin = calculateFactorial(n) / (calculateFactorial(k) * calculateFactorial(n - k));
+		println(combin);
+	}
+	
+	private int calculateFactorial(int number) {
+		int factorial = 1;
+		for (int i = 1; i <= number; i++) {
+			factorial *= i;
+		}
+		return factorial;
+	}
 }
