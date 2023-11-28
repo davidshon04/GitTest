9adacd133c2f0093668c1bf9232c2f3435eb413c
S <s>
2023-11-29 00:30:49 +0400

    {"types":"C","runs":24}

--- L11SL31.java
+++ L11SL31.java
@@ -1,4 +1,33 @@
+import java.awt.Color;
+import java.awt.event.MouseEvent;
 
-public class L11SL31 {
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
+// create square which moves along mouse
+
+public class L11SL31 extends GraphicsProgram {
+	
+	private static final int SIZE = 30;
+	private GRect square;
+	
+	public void run() {
+		square = createSquare();
+		addMouseListeners();
+	}
+	
+	public void mouseMoved(MouseEvent e) {
+		double dx = e.getX() - square.getX() - SIZE / 2;
+		double dy = e.getY() - square.getY() - SIZE / 2;
+		square.move(dx, dy);
+		pause(15);
+	}
+
+	private GRect createSquare() {
+		square = new GRect(SIZE, SIZE);
+		square.setFilled(true);
+		square.setColor(Color.CYAN);
+		add(square, (getWidth() - SIZE) / 2, (getHeight() - SIZE) / 2);
+		return square;
+	}
 }
