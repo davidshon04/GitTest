8a9499100503114109e81b73637931e809e2cf14
S <s>
2023-11-28 17:03:42 +0400

    {"types":"A","runs":0}

---
+++ Problem46.java
@@ -0,0 +1,29 @@
+import acm.program.ConsoleProgram;
+
+//46. დაწერეთ მეთოდი რომელსაც გადაეცემა სტიგნი და რომელიც აბრუნებს ამ სტრინგში
+//ყველაზე ხშირ სიმბოლოს.
+
+public class Problem46 extends ConsoleProgram {
+	public void run() {
+//		String text = readLine("Enter text: ");
+//	
+//		int maxCount = 0;
+//		char maxChar = 0;
+//		for(int i = 0; i < text.length(); i++) {
+//			int numOcc = numOccurences(text, text.charAt(i));
+//			if(numOcc > maxCount) {
+//				maxCount = numOcc;
+//				maxChar = 
+//			}
+//		}
+//	}
+//	
+//	private void numOccurences(String text, char symbol) {
+//		int n = 0;
+//		for (int i = 0; i < text.length(); i++) {
+//			if (symbol == text.charAt(i)) {
+//				n++;
+//			}
+//		}
+	}
+}
