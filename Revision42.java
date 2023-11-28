9c306fdd4dcb172b76f432cb0dea53b5e05eafca
S <s>
2023-11-29 01:38:50 +0400

    {"types":"C","runs":4}

--- Revision42.java
+++ Revision42.java
@@ -1,4 +1,26 @@
+import java.awt.event.MouseEvent;
 
-public class Revision42 {
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
 
+//42. Paint-ის მონაკვეთის ხატვის გაკეთება, ჯერ აჭერ მაუსს, ფიქსირდება ერთი წვერო, მერე
+//მაუსის მოძრაობისას მონაკვეთიც მოძრაობს რადგან მეორე წვერო არაა დაფიქსირებული და
+//კლიკით ფიქსირდება მეორე წვეროც.
+
+public class Revision42 extends GraphicsProgram {
+	
+	private GLine line;
+	
+	public void run() {
+		addMouseListeners();
+	}
+	
+	public void mouseClicked(MouseEvent e) {
+		line = new GLine(e.getX(), e.getY(), e.getX(), e.getY());
+		add(line);
+	}
+	
+	public void mouseDragged(MouseEvent e) {
+		line.setEndPoint(e.getX(), e.getY());
+	}
 }
