113325eb7be96b60d31372c9cadd74a0fbf8aaa8
S <s>
2023-11-29 00:01:40 +0400

    {"types":"C","runs":17}

--- Dam13Pr3.java
+++ Dam13Pr3.java
@@ -1,4 +1,22 @@
+import java.awt.Color;
 
-public class Dam13Pr3 {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
+//გრაფიკულ პროგრამაში შექმენით რანდომ ფერის ბურთი, რომელიც დამატებულია ეკრანის ცენტრში. 
+//ბურთს მოძრაობა დააწყებინეთ რანდომ მიმართულებით. კედლებთან დაჯახებისას ბურთი უნდა ირეკლებოდეს.
+
+public class Dam13Pr3 extends GraphicsProgram {
+	
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	private static final int DIAMETER = 30;
+	
+	
+	public void run() {
+		GOval circle = new GOval(DIAMETER, DIAMETER);
+		circle.setFilled(true);
+		Color color = rgen.nextColor();
+		circle.setColor(color);
+	}
 }
