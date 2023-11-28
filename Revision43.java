26c344b950114884fd3001bd13cbcb40e436acf9
S <s>
2023-11-29 01:53:20 +0400

    {"types":"C","runs":6}

--- Revision43.java
+++ Revision43.java
@@ -1,4 +1,45 @@
+import java.awt.Color;
+import java.awt.event.MouseEvent;
 
-public class Revision43 {
+import acm.graphics.GObject;
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
+//43. წრეწირის drag ით გადაადგილება
+
+public class Revision43 extends GraphicsProgram {
+	
+	private GOval circle;
+	private int prevX;
+	private int prevY;
+	
+	public void run() {
+		addCircle();
+		addMouseListeners();
+	}
+	
+	public void mousePressed(MouseEvent e) {
+		GObject obj = getElementAt(e.getX(), e.getY());
+		if (obj != null) {
+			prevX = e.getX();
+			prevY = e.getY();
+		}		
+	}
+	
+	public void mouseDragged(MouseEvent e) {
+		GObject obj = getElementAt(e.getX(), e.getY());
+		if (obj != null) {
+			obj.move(e.getX() - obj.getX(), e.getY() - obj.getY());
+		}
+	}
+
+	private GOval addCircle() {
+		circle = new GOval (50, 50);
+		circle.setFilled(true);
+		circle.setColor(Color.ORANGE);
+		add(circle);
+		return circle;
+	}
+	
+	
 }
