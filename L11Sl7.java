6e919f6ce9b2ebb4d177e7e3427807f7660d44ae
S <s>
2023-11-29 00:20:02 +0400

    {"types":"C","runs":23}

--- L11Sl7.java
+++ L11Sl7.java
@@ -1,13 +1,16 @@
+import java.awt.Color;
 import java.awt.event.MouseEvent;
 
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
 // Hole puncher
 
 public class L11Sl7 extends GraphicsProgram {
 	
-	private static final int RADIUS = 20;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	private static final int RADIUS = 15;
 	
 	public void run() {
 		addMouseListeners();
@@ -16,6 +19,8 @@ public class L11Sl7 extends GraphicsProgram {
 	public void mouseClicked(MouseEvent e) {
 		GOval circle = new GOval(2 * RADIUS, 2 * RADIUS);
 		circle.setFilled(true);
+		Color color = rgen.nextColor();
+		circle.setColor(color);
 		int x = e.getX() - RADIUS;
 		int y = e.getY() - RADIUS;
 		add(circle, x, y);
