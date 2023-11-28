4438ae922f88321befb6d7dfcb78ef2b1a1b67a8
S <s>
2023-11-29 00:18:18 +0400

    {"types":"C","runs":22}

--- L11Sl7.java
+++ L11Sl7.java
@@ -1,4 +1,24 @@
+import java.awt.event.MouseEvent;
 
-public class L11Sl7 {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+
+// Hole puncher
+
+public class L11Sl7 extends GraphicsProgram {
+	
+	private static final int RADIUS = 20;
+	
+	public void run() {
+		addMouseListeners();
+	}
+	
+	public void mouseClicked(MouseEvent e) {
+		GOval circle = new GOval(2 * RADIUS, 2 * RADIUS);
+		circle.setFilled(true);
+		int x = e.getX() - RADIUS;
+		int y = e.getY() - RADIUS;
+		add(circle, x, y);
+	}
 
 }
