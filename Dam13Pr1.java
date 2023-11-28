5a624085e176626f690fed98310b8f28673a92e5
S <s>
2023-11-28 23:37:23 +0400

    {"types":"C","runs":4}

--- Dam13Pr1.java
+++ Dam13Pr1.java
@@ -5,14 +5,21 @@ import acm.program.GraphicsProgram;
 //ეკრანის ზომის ცვლილებისას ხაზი ყოველთვის ეკრანის შუაში უნდა იყოს, ეკრანის მთელს სიგრძეზე.
 
 public class Dam13Pr1 extends GraphicsProgram {
+	
+	GLine line;
+	
 	public void run() {
-		GLine line = new GLine(0, getHeight() / 2, getWidth(), getHeight() / 2);
-		add(line);
+		createLine();
 		while (true) {
 			remove(line);
-			line = new GLine(0, getHeight() / 2, getWidth(), getHeight() / 2);
-			add(line);
+			createLine();
 			pause(20);
 		}
+	}
+	
+	private GLine createLine() {
+		line = new GLine(0, getHeight() / 2, getWidth(), getHeight() / 2);
+		add(line);
+		return line;
 	}
 }
