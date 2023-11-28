8e754f62bee6f956099d4a631d00d56ab16d8e2b
S <s>
2023-11-28 17:10:50 +0400

    {"types":"C","runs":1}

--- Problem50.java
+++ Problem50.java
@@ -1,4 +1,19 @@
+import java.util.StringTokenizer;
 
-public class Problem50 {
+import acm.program.ConsoleProgram;
 
+//50. შეყვანილ ტექსტში დათვალეთ სიტვყების რაოდენობა, ტოკენაიზერის საშუალებით. 
+
+public class Problem50 extends ConsoleProgram {
+	public void run() {
+		String text = readLine("Enter text: ");
+		StringTokenizer tokenizer = new StringTokenizer(text);
+		
+		int wordCount = 0;
+		while (tokenizer.hasMoreTokens()) {
+			String currToken = tokenizer.nextToken();
+			wordCount++;
+		}
+		println("Count: " + wordCount);
+	}
 }
