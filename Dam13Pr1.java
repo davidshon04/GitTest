c91755bec956acaa9bc8f36ba8b51df1fe15d5e7
S <s>
2023-11-28 23:32:38 +0400

    {"types":"C","runs":1}

--- Dam13Pr1.java
+++ Dam13Pr1.java
@@ -1,4 +1,21 @@
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
 
-public class Dam13Pr1 {
+//დაწერეთ გრაფიკული პროგრამა, რომლიც ეკრანის შუაში გაავლებს ჰორიზონტალურ ხაზს. 
+//ეკრანის ზომის ცვლილებისას ხაზი ყოველთვის ეკრანის შუაში უნდა იყოს, ეკრანის მთელს სიგრძეზე.
 
+public class Dam13Pr1 extends GraphicsProgram {
+	public void run() {
+		GLine line = createLine();
+		while(true) {
+			remove(line);
+			createLine();
+		}
+	}
+
+	private GLine createLine() {
+		GLine line = new GLine(0, getHeight() / 2, getWidth(), getHeight() / 2);
+		add(line);
+		return line;
+	}
 }
