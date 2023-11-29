3a6e323279229bd64c2857b4c2c7bedb8522d411
S <s>
2023-11-30 00:32:06 +0400

    {"types":"C","runs":2}

--- L12SL15.java
+++ L12SL15.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
 
-public class L12SL15 {
+//String operations
 
+public class L12SL15 extends ConsoleProgram {
+	public void run() {
+		String text = "I am Starboy!";
+		
+		// print the length
+		println(text.length());
+		
+		// print first letter
+		char firstSymbol = text.charAt(0);
+		println("Fisrt symbol is: " + firstSymbol);
+		
+		// printline each symbol
+		for (int i = 0; i < text.length(); i++) {
+			println(text.charAt(i));
+		}
+	}
 }
