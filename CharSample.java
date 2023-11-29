fbeed6e1d608d7e8a6b31c45f911cb84a9a8b131
S <s>
2023-11-30 00:47:42 +0400

    {"types":"C","runs":4}

--- CharSample.java
+++ CharSample.java
@@ -1,4 +1,24 @@
+import acm.program.ConsoleProgram;
 
-public class CharSample {
+public class CharSample extends ConsoleProgram {
+	public void run() {
+		// determine the first symbol is a letter or not.
+		String text = readLine("Enter text: ");
+		char firstCh = text.charAt(0);
+		if (Character.isLetter(firstCh)) {
+			println("It's a letter!");
+		} else {
+			println("It's not a letter.");
+		}
+		
+		// determine the symbol is uppercase or not
+		if (Character.isUpperCase(firstCh)) {
+			println("UpperCase");
+			println(firstCh + "-->" + Character.toLowerCase(firstCh));
+		} else {
+			println("LowerCase");
+			println(firstCh + "-->" + Character.toUpperCase(firstCh));
+		}
+	}
 
 }
