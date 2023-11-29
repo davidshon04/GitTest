a7028ed24391d6fe9e0627bf177e36da23557da8
S <s>
2023-11-30 01:14:30 +0400

    {"types":"C","runs":9}

--- L12SL80.java
+++ L12SL80.java
@@ -1,4 +1,27 @@
+import acm.program.ConsoleProgram;
 
-public class L12SL80 {
+// Create a method that tests palyndromes
 
+public class L12SL80 extends ConsoleProgram {
+	public void run() {
+		while(true){
+		String text = readLine("Enter text: ");
+		if (isPalyndrome(text)) {
+			println("Palyndrome");
+		} else {
+			println("just a text");
+		}
+		}
+	}
+
+	private boolean isPalyndrome(String text) {
+		for (int i = 0; i < text.length() / 2; i++) {
+			char fCh = text.charAt(i);
+			char lCh = text.charAt(text.length() - i - 1);
+			if (fCh == lCh) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
