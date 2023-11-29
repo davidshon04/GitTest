6701191bb35f941cb484c747bf46f94f25a279dc
S <s>
2023-11-30 01:29:17 +0400

    {"types":"C","runs":12}

--- Revision45.java
+++ Revision45.java
@@ -1,4 +1,20 @@
+import acm.program.ConsoleProgram;
 
-public class Revision45 {
+//45. მომხმარებელს კონსოლიდან შეჰყავს ტექსტი და სიმბოლო, თქვენმა პროგრამამ უნდა იპოვოს
+//რამდენჯერ გვხვდება მოცემულ ტექსტში მოცემული სიმბოლო
 
+public class Revision45 extends ConsoleProgram {
+	public void run() {
+		String text = readLine("Enter text: ");
+		String symbol = readLine("Enter symbol: ");
+		char symb = symbol.charAt(0);
+		
+		int chFreq = 0;
+		for (int i = 0; i < text.length(); i++) {
+			if (text.charAt(i) == symb) {
+				chFreq++;
+			}
+		}
+		println(symb + " is used " + chFreq + " times.");
+	}
 }
