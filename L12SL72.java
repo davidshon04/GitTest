7c86f29984720c13f4babceb95e6959ff2350dbc
S <s>
2023-11-30 01:00:35 +0400

    {"types":"C","runs":7}

--- L12SL72.java
+++ L12SL72.java
@@ -1,4 +1,16 @@
+import acm.program.ConsoleProgram;
 
-public class L12SL72 {
+//Reverse a String
 
+public class L12SL72 extends ConsoleProgram {
+	public void run() {
+		String text = readLine("Enter text: ");
+
+		String reversed = "";
+		for (int i = text.length(); i > 0; i--) {
+			char ch = text.charAt(i - 1);
+			reversed += ch;
+		}
+		println(reversed);
+	}
 }
