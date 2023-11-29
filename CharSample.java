0775148fd92dd2c5ad75224057cb1ba274c7894a
S <s>
2023-11-30 00:49:12 +0400

    {"types":"C","runs":6}

--- CharSample.java
+++ CharSample.java
@@ -15,7 +15,7 @@ public class CharSample extends ConsoleProgram {
 		if (Character.isUpperCase(firstCh)) {
 			println("UpperCase");
 			println(firstCh + "-->" + Character.toLowerCase(firstCh));
-		} else {
+		} else if (Character.isLowerCase(firstCh)) {
 			println("LowerCase");
 			println(firstCh + "-->" + Character.toUpperCase(firstCh));
 		}
