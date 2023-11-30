cb23c74fde632a6ea9a92bbd2bec81cdefd6309a
S <s>
2023-11-30 14:15:46 +0400

    {"types":"C","runs":3}

--- L12Sl32.java
+++ L12Sl32.java
@@ -6,17 +6,20 @@ public class L12Sl32 extends ConsoleProgram {
 	public void run() {
 		String text = readLine("Enter text: ");
 		int cipherKey = readInt("Enter key: ");
-		
+
 		String cipheredMessage = cipherMessage(text, cipherKey);
 		println(cipheredMessage);
 	}
 
+	
+	
+	//unfinished
 	private String cipherMessage(String text, int cipherKey) {
 		String cipheredMessage = "";
 		for (int i = 0; i < text.length(); i++) {
 			char Ch = text.charAt(i);
-			int index = (int) Ch + cipherKey;
-			cipheredMessage += (char) index;
+				int index = (int) Ch + cipherKey;
+				cipheredMessage += (char) index;
 		}
 		return cipheredMessage;
 	}
