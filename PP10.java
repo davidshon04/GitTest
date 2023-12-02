d0cddbb83406ecfb0adfff54e677ad5a265e270d
S <s>
2023-12-02 16:21:10 +0400

    {"types":"C","runs":75}

--- PP10.java
+++ PP10.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
 
-public class PP10 {
+//დაწერეთ მეთოდი, რომელსაც გადმოეცემა რიცხვი და აბრუნებს ამ რიცხვის კენტ ადგილას მყოფი ციფრთა ჯამი 
+//უდრის თუ არა ლუწ ადგილას მდგომ ციფრთა ჯამს.
+//მაგ: 1232 -> true  1234 -> false (Practice10)
+//Tags: Math, IntegerDigits
 
+public class PP10 extends ConsoleProgram {
+	public void run() {
+		int num = readInt("Enter number: ");
+		
+	}
+	
+	private boolean sumsChecker(int num) {
+		if (oddSum(num) == evenSum(num)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
