26e3618bc80572076a34a40e0f81aa5eeba3cab0
S <s>
2023-12-02 16:40:34 +0400

    {"types":"C","runs":1}

--- PP16.java
+++ PP16.java
@@ -1,4 +1,18 @@
+import acm.program.ConsoleProgram;
 
-public class PP16 {
+//16. მოხმარებელს შეყავს მთელი რიცხვი n, a, q, პროგრამამ უნდა დაბეჭდოს გეომეტრიული პროგრესიის პირველი n ცალი წევრი. 
+//a არის გეომეტრიული პროგრესიის პირველი წევრი, ხოლო q გეომეტიული მნიშვნელი. (Practice16)
+//Tags: Math, Sequence
 
+public class PP16 extends ConsoleProgram {
+	public void run() {
+		int n = readInt("Enter number: ");
+		int a = readInt("Enter number: ");
+		int q = readInt("Enter number: ");
+
+		for(int i = 0; i < n; i++) {
+			println("number " + (i+1) + " = " + a * Math.pow(q, i));
+		}
+	}
+	
 }
