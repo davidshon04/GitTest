9b5181d14124788120962fb9e9edc6c587a90bdc
S <s>
2023-12-02 15:56:36 +0400

    {"types":"C","runs":22}

--- PP7.java
+++ PP7.java
@@ -1,4 +1,27 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class PP7 {
+//7.გაქვს 20 ლარი და თამაშობ მონეტის აგდებას, ყოველ ჯერზე დებს გერბზე 2 ლარს, წაგების შემთხვევაში კარგავს ამ 2 ლარს, 
+//ხოლო მოგების შემთხვევაში იბრუნებს დადებულ ფსონს და დამატებით 1 ლარს იგებს.  საშუალოდ რა თანხა გექნება 100 აგდების შემდეგ
+//(თუ თანხა გაგითავდა, ვთქვთ მე–20 აგდებაზე შენი ექსპერიმენტი რჩება და 21-ედ ვეღარ ააგდებ. ასეთი ექსპერიმენტი შედეგებში ჩვეულებრივ უნდა გაითვალისწინო)  ? 
+//საშუალოს გამოსათვლელად ჩაატარეთ 100 ჯერ(ან ნაკლებჯერ, თუ გაკოტრდი) აგდების ექსპერიმენტი 10000 ჯერ. (Practice07)
+//Tags: RandomGenerator, Simulations/Experiments , AverageResult, Coin
 
+public class PP7 extends ConsoleProgram {
+	
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	private static final int NUM_FLIPS = 100;
+	
+	public void run() {
+		int totalCashBalance = 20;
+		for (int i = 0; i < NUM_FLIPS; i++) {
+			boolean gerbi = rgen.nextBoolean();
+			if(gerbi) {
+				totalCashBalance += 1;
+			} else {
+				totalCashBalance -= 1;
+			}
+		}
+		println(totalCashBalance);
+	}
 }
