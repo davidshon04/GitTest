d0152fda7d164a6f506f84060364367df331d907
S <s>
2023-12-02 16:07:52 +0400

    {"types":"C","runs":49}

--- PP7.java
+++ PP7.java
@@ -30,7 +30,7 @@ public class PP7 extends ConsoleProgram {
 			if(gerbi) {
 				startingBalance += 1;
 			} else {
-				startingBalance -= 2;
+				startingBalance -= 1;
 				if (startingBalance <= 0) {
 					break;
 				}
