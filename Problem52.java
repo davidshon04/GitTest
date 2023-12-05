de2695cd29a258b209db6f0139773d7f8880f1af
S <s>
2023-12-05 17:22:58 +0400

    {"types":"C","runs":1}

--- Problem52.java
+++ Problem52.java
@@ -1,4 +1,37 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class Problem52 {
+import acm.program.ConsoleProgram;
 
+public class Problem52 extends ConsoleProgram{
+	
+	private static final String FILE_NAME = "Problem52.java";
+	
+	public void run() {
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME));
+			
+			println("opened reader");
+			while(true) {
+				String newLine = reader.readLine();
+				if (newLine == null) {
+					break;
+				}
+				println(newLine);
+			}
+			reader.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			println("შეცდომა მოხდა, ფაილი ვერ მოიძებნა");
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			println("სხვა შეცდომა მოხდა");
+
+		}
+	}
 }
