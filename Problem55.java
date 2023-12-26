961ca437c0d22f1cd14915d04ded02bbc366be26
S <s>
2023-12-27 00:14:12 +0400

    {"types":"C","runs":3}

--- Problem55.java
+++ Problem55.java
@@ -1,4 +1,28 @@
+import java.util.Arrays;
 
-public class Problem55 {
+import acm.program.ConsoleProgram;
 
+//55. კონსოლიდან შეგყვავს n და შემდეგ n ცალი მთელი რიცხვი, გაარკვიეთ შეყვანილი რიცხვები
+//არის თუ არა ზრდადობით დალაგებული. თუკი არის დაბეჭდეთ “sorted” თუ არა მაშინ
+//დაბეჭდეთ “not sorted”.
+
+public class Problem55 extends ConsoleProgram {
+
+	public void run() {
+		int n = readInt("Enter value: ");
+		int[] numbers = new int[n];
+		int[] sorted = new int[n];
+		for(int i = 0; i < numbers.length; i++) {
+			int num = readInt("Enter number: ");
+			numbers[i] = num;
+			sorted[i] = num;
+		}
+		Arrays.sort(sorted);
+		
+		if (Arrays.equals(numbers, sorted)) {
+			println("sorted");
+		} else {
+			println("not sorted");
+		}
+	}
 }
