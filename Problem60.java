b318222c6e56c642090f4f9189f21fc1156044aa
S <s>
2023-12-27 01:34:06 +0400

    {"types":"C","runs":17}

--- Problem60.java
+++ Problem60.java
@@ -1,4 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 
-public class Problem60 {
+import acm.program.ConsoleProgram;
 
+//60. კონსოლიდან შეგვყავს რიცხვები მანამ სანამ არ შეიყვანთ -1-ს, დაბეჭდეთ შეყვანილი
+//რიცხვები შებრუნებული მიმდევრობით.
+
+public class Problem60 extends ConsoleProgram {
+	
+	private static final int SENTINEL = -1;
+	
+	public void run() {
+		ArrayList<Integer> numberList = new ArrayList<Integer>();
+		while(true) {
+			int n = readInt("Enter number: ");
+			if (n == SENTINEL) {
+				break;
+			}
+			numberList.add(n);
+		}
+		
+		int[] reversedNumbers = new int[numberList.size()];
+		for (int i = 0; i < reversedNumbers.length; i++) {
+			reversedNumbers[i] = numberList.get(numberList.size() - i - 1);
+		}
+		
+		println("Reversed: " + Arrays.toString(reversedNumbers));
+	}
 }
