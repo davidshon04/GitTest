8f59fd53ff1e17d2c1147fc775bcc6073c896b68
S <s>
2024-01-12 19:24:56 +0400

    {"types":"C","runs":7}

--- ButtonSample2.java
+++ ButtonSample2.java
@@ -1,4 +1,31 @@
+import java.awt.event.ActionEvent;
 
-public class ButtonSample2 {
+import javax.swing.JButton;
 
+import acm.program.ConsoleProgram;
+
+public class ButtonSample2 extends ConsoleProgram {
+	private String str1 = "Hello";
+	private String str2 = "David";
+	private String str3 = "Midterms";
+
+	public void init() {
+		setFont("Courier-24");
+		
+		add(new JButton(str1), NORTH);
+		add(new JButton(str2), SOUTH);
+		add(new JButton(str3), SOUTH);
+		addActionListeners();
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		String cmd = e.getActionCommand();
+		if (cmd.equals(str1)) {
+			println("Hello World");
+		} else if (cmd.equals(str2)) {
+			println("David Shonia");
+		} else if (cmd.equals(str3)) {
+			println("Midterms VS Finals");
+		}
+	}
 }
