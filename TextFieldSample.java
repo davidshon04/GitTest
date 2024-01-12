80ff5502d780ceacb66d55744c57db4cd56baed5
S <s>
2024-01-12 19:56:32 +0400

    {"types":"C","runs":8}

--- TextFieldSample.java
+++ TextFieldSample.java
@@ -1,4 +1,22 @@
+import java.awt.event.ActionEvent;
 
-public class TextFieldSample {
+import javax.swing.JTextField;
 
+import acm.program.ConsoleProgram;
+
+public class TextFieldSample extends ConsoleProgram {
+
+	private JTextField tf;
+	
+	public void init() {
+		tf = new JTextField(15);
+		tf.addActionListener(this);
+		add(tf, SOUTH);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == tf) {
+			println("Hello" + tf.getText());
+		}
+	}
 }
