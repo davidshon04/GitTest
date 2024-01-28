6d488cad361d047de8bf53de3e6654b4c273b43d
S <s>
2024-01-29 01:49:15 +0400

    {"types":"C","runs":14}

--- Problem59.java
+++ Problem59.java
@@ -28,7 +28,19 @@ public class Problem59 extends ConsoleProgram {
 	
 	public void run() {
 		fillMatrix(matrix);
-		println(matrix.toString());
+		checkRects(matrix);
+	}
+
+	private int checkRects(int[][] matrix2) {
+		int maxRectSize = 0;
+		for (int i = 0; i < matrix2.length; i++) {
+			for (int j = 0; j < matrix2[0].length; j++) {
+				if (matrix2[i][j] == 1) {
+					
+				}
+			}
+		}
+		return maxRectSize;
 	}
 
 	private void fillMatrix(int[][] matrix2) {
