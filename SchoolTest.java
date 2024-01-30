35a5564bb1e421990a159600c9613a292609674c
S <s>
2024-01-31 01:03:07 +0400

    {"types":"C","runs":1}

--- SchoolTest.java
+++ SchoolTest.java
@@ -1,4 +1,34 @@
+import acm.program.ConsoleProgram;
 
-public class SchoolTest {
+public class SchoolTest extends ConsoleProgram {
 
+	public void run() {
+		School school = new School();
+		
+		school.addTeacher("Tsiala");
+		school.addTeacher("Dali");
+		school.addTeacher("Zamira");
+		school.addTeacher("Jilda");
+		
+		school.addSubject("Dali", "Georgian");
+		school.addSubject("Jilda", "Georgian");
+		school.addSubject("Mtvarisa", "Georgian");
+		school.addSubject("Tsiala", "Georgian");
+		school.addSubject("Tsiala", "History");
+		school.addSubject("Zamira", "Art");
+		school.addSubject("Mzeona", "Geography");
+		
+		school.addPupil("Dato", "Geography");
+		school.addPupil("Kristi", "Geography");
+		school.addPupil("Dato", "Georgian");
+		school.addPupil("Kristi", "History");
+		school.addPupil("Guka", "Art");
+		school.addPupil("Gigi", "Art");
+		school.addPupil("Dato", "History");
+		
+		println("Dato: " + school.getTeachers("Dato"));
+		println("Tsiala:" + school.getPupils("Tsiala"));
+		
+
+	}
 }
