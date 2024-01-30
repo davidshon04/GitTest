a9a03f1d0efc7378d03f1c7b67087c11e58ad7be
S <s>
2024-01-30 21:55:40 +0400

    {"types":"C","runs":38}

--- ComponentListenerSample.java
+++ ComponentListenerSample.java
@@ -1,4 +1,41 @@
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 
-public class ComponentListenerSample {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
+public class ComponentListenerSample extends GraphicsProgram implements ComponentListener {
+
+	private GOval oval;
+
+	public void init() {
+		oval = new GOval(getWidth() / 2, getHeight() / 2);
+		add(oval, getWidth() / 4, getHeight() / 4);
+		addComponentListener(this);
+	}
+
+	@Override
+	public void componentHidden(ComponentEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void componentMoved(ComponentEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void componentResized(ComponentEvent e) {
+		removeAll();
+		oval = new GOval(getWidth() / 2, getHeight() / 2);
+		add(oval, getWidth() / 4, getHeight() / 4);
+	}
+
+	@Override
+	public void componentShown(ComponentEvent e) {
+		// TODO Auto-generated method stub
+
+	}
 }
