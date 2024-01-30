54174fa312abe0e06e995a73197a254ff89351db
S <s>
2024-01-30 16:33:24 +0400

    {"types":"C","runs":5}

--- Problem61.java
+++ Problem61.java
@@ -1,4 +1,41 @@
+import acm.program.ConsoleProgram;
 
-public class Problem61 {
+//61. კონსოლიდან შეგვყავს რიცხვი m და n, შემდეგ n ცალი რიცხვი. პროგრამამ უნდა იპოვოს
+//შეყვანილი რიცხვებიდან წყვილი რომელთა ჯამი არის m.
 
+public class Problem61 extends ConsoleProgram {
+	
+	public void run() {
+		int m = readInt("Enter m: ");
+		int n = readInt("Enter n: ");
+		int[] nums = new int[n];
+		for (int i = 0; i < n; i++) {
+			int numbers = readInt("Enter numbers: ");
+			nums[i] = numbers;
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = i+1; j < n; j++) {
+				if (nums[i] + nums[j] == m) {
+					println(nums[i] + " + " + nums[j] + " = " + m);
+				} else {
+					println("No such numbers");
+				}
+			}
+		}
+		
+		//findCouple(m, n, nums);
+	}
+
+//	private void findCouple(int m, int n, int[] nums) {
+//		for (int i = 0; i < n; i++) {
+//			for (int j = i+1; j < n; j++) {
+//				if (nums[i] + nums[j] == m) {
+//					println(nums[i] + " + " + nums[j] + " = " + m);
+//				} else {
+//					println("No such numbers");
+//				}
+//			}
+//		}
+//		
+//	}
 }
