78198dba7d7d02894e275e3ae5a1675f5cb47437
S <s>
2024-01-30 16:13:52 +0400

    {"types":"C","runs":1}

--- Problem59.java
+++ Problem59.java
@@ -20,34 +20,46 @@ import acm.util.RandomGenerator;
 //გადმოცემულ მატრიცაში ერთი უჯრა მაინც იქნება რომელშიც წერია 1.
 
 public class Problem59 extends ConsoleProgram {
-
-	private static final int N = 3;
-	private static final int M = 4;
-	private int[][] matrix = new int[N][M];
-	private RandomGenerator rgen = RandomGenerator.getInstance();
 	
 	public void run() {
-		fillMatrix(matrix);
+		int[][] matrix = new int[][] {
+			{0,1,1,1,0,0,0},
+			{1,1,0,0,1,1,0},
+			{1,1,0,1,1,0,1},
+			{0,0,1,1,1,1,0},
+			{1,1,0,1,1,0,1}
+		};
+		
 		checkRects(matrix);
 	}
 
 	private int checkRects(int[][] matrix2) {
 		int maxRectSize = 0;
+		//i and j are the starting points
 		for (int i = 0; i < matrix2.length; i++) {
 			for (int j = 0; j < matrix2[0].length; j++) {
-				if (matrix2[i][j] == 1) {
-					
+				//i0 and j0 are the ending points
+				for (int i0 = i; i0 < matrix2.length; i0++) {
+					for (int j0 = j; j0 < matrix2[0].length; j0++) {
+						if (rectWithOnes(matrix2, i, j, i0, j0)) {
+							int currRectSize = (i0 - i + 1) * (j0 - j + 1);
+							maxRectSize = Math.max(currRectSize, maxRectSize);
+						}
+					}
 				}
 			}
 		}
 		return maxRectSize;
 	}
 
-	private void fillMatrix(int[][] matrix2) {
-		for (int i = 0; i < matrix2.length; i++) {
-			for (int j = 0; j < matrix2[0].length; j++) {
-				matrix2[i][j] = rgen.nextInt(0, 1);
+	private boolean rectWithOnes(int[][] matrix2, int i, int j, int i0, int j0) {
+		for (int m = i; m <= i0; m++) {
+			for (int n = j; n <= j0; n++) {
+				if (matrix2[m][n] != 1) {
+					return false;
+				}
 			}
 		}
+		return true;
 	}
 }
