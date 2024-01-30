31efd3e41e58706b41abd346f789dff654bfbabb
S <s>
2024-01-30 19:05:23 +0400

    {"types":"C","runs":19}

--- Problem65.java
+++ Problem65.java
@@ -1,4 +1,48 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class Problem65 {
+import acm.program.ConsoleProgram;
 
+//65. 63-ე ამოცანის გაგრძელება. დაბეჭდეთ ადამიანი რომელსაც ყველაზე მეტი ადამიანი
+//ემეგობრება
+
+public class Problem65 extends ConsoleProgram {
+	private static final String SENTINEL = "";
+
+	public void run() {
+		HashMap<String, ArrayList<String>> friendsMap = new HashMap<String, ArrayList<String>>();
+		
+		while (true) {
+			String friend = readLine("Friend: ");
+			String person = readLine("Person: ");
+			if (friend.equals(SENTINEL) || person.equals(SENTINEL)) {
+				break;
+			}
+			
+			if (!friendsMap.containsKey(person)) {
+				friendsMap.put(person, new ArrayList<String>());
+			}
+			
+			ArrayList<String> currFriends = friendsMap.get(person);
+			if (!currFriends.contains(friend)) {
+				currFriends.add(friend);
+			}
+			
+			friendsMap.put(person, currFriends);
+		}
+		println(friendsMap);
+		
+		HashMap<String, Integer> friendsFrequency = new HashMap<String, Integer>();
+		for (String person: friendsMap.keySet()) {
+			ArrayList<String> friendList = friendsMap.get(person);
+			for (int i = 0; i < friendList.size(); i++) {
+				if (!friendsFrequency.containsKey(friendList.get(i))) {
+					friendsFrequency.put(friendList.get(i), 1);
+				} else {
+					friendsFrequency.replace(friendList.get(i), friendsFrequency.get(friendList.get(i)), friendsFrequency.get(friendList.get(i))+1);
+				}
+			}
+		}
+		println(friendsFrequency);
+	}
 }
