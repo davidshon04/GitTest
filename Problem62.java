e17902294892c995c71f3310233dbf2ce1d4af2b
S <s>
2024-01-30 17:00:36 +0400

    {"types":"C","runs":10}

--- Problem62.java
+++ Problem62.java
@@ -1,4 +1,29 @@
+import java.util.HashMap;
 
-public class Problem62 {
+import acm.program.ConsoleProgram;
 
+//62. კონსოლიდან შეგვყავს სახელები, მანამ სანამ არ შევიყვანთ ცარიელ სტრინგს. დაბეჭდეთ
+//სახელი და მის გასწვრივ რაოდენობა რამდენჯერ შევიყვანეთ ეს სახელი.
+
+public class Problem62 extends ConsoleProgram {
+
+	private static final String SENTINEL = "";
+	
+	public void run() {
+		HashMap<String, Integer> namesMap = new HashMap<String, Integer>();
+		
+		while (true) {
+			String name = readLine("Enter name: ");
+			if (name.equals(SENTINEL)) {
+				break;
+			}
+			if (!namesMap.containsKey(name)) {
+				namesMap.put(name, 1);
+			} else {
+				namesMap.replace(name, namesMap.get(name), namesMap.get(name) + 1);
+			}
+		}
+		
+		println(namesMap);
+	}
 }
