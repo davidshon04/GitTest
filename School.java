bea68c883e944d5ad26319bc1a0f83a269ff5f05
S <s>
2024-01-31 17:21:59 +0400

    {"types":"C","runs":1}

--- School.java
+++ School.java
@@ -1,110 +1,79 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 
 public class School {
 
-	private HashMap<String, ArrayList<String>> teachersSubjects;
-	private HashMap<String, ArrayList<String>> subjectsTeachers;
-	private HashMap<String, ArrayList<String>> subjectsStudents; 
-	private HashMap<String, ArrayList<String>> studentsSubjects;
+	private HashMap<String, HashSet<String>> teachersSubjects;
+	private HashMap<String, HashSet<String>> subjectsTeachers;
+	private HashMap<String, HashSet<String>> subjectsStudents; 
+	private HashMap<String, HashSet<String>> studentsSubjects;
 	
 	public School() {
-		teachersSubjects = new HashMap<String, ArrayList<String>>();
-		subjectsTeachers = new HashMap<String, ArrayList<String>>();
-		subjectsStudents = new HashMap<String, ArrayList<String>>();
-		studentsSubjects = new HashMap<String, ArrayList<String>>();
+		teachersSubjects = new HashMap<String, HashSet<String>>();
+		subjectsTeachers = new HashMap<String, HashSet<String>>();
+		subjectsStudents = new HashMap<String, HashSet<String>>();
+		studentsSubjects = new HashMap<String, HashSet<String>>();
 	}
 	
 	public void addTeacher(String teacher) {
-		if (!teachersSubjects.containsKey(teacher)) {
-			teachersSubjects.put(teacher, new ArrayList<String>());
-		}
+		teachersSubjects.putIfAbsent(teacher, new HashSet<String>());
 	}
 	
 	public void addSubject(String teacher, String subject) {
 		if (teachersSubjects.containsKey(teacher)) {
-			ArrayList<String> subjList = teachersSubjects.get(teacher);
-			if (!subjList.contains(subject)) {
-				subjList.add(subject);
-			}
-			teachersSubjects.put(teacher, subjList);
-		}
-		
-		if (subjectsTeachers.containsKey(subject)) {
-			ArrayList<String> teachList = subjectsTeachers.get(subject);
-			if (!teachList.contains(teacher)) {
-				teachList.add(teacher);
-			}
-			subjectsTeachers.put(subject, teachList);
+			teachersSubjects.get(teacher).add(subject);
+			if (!subjectsTeachers.containsKey(subject)) {
+				subjectsTeachers.put(subject, new HashSet<String>());
+			} 
+			subjectsTeachers.get(subject).add(teacher);
+			subjectsStudents.putIfAbsent(subject, new HashSet<String>());
 		}
 	}
 	
 	public void addPupil(String pupil, String subject) {
 		if (subjectsStudents.containsKey(subject)) {
-			ArrayList<String> studList = subjectsStudents.get(subject);
-			if (!studList.contains(pupil)) {
-				studList.add(pupil);
-			}
-			subjectsStudents.put(subject, studList);
+			subjectsStudents.get(subject).add(pupil);
+			if (!studentsSubjects.containsKey(pupil)) {
+				studentsSubjects.put(pupil, new HashSet<String>());
+			} 
+			studentsSubjects.get(pupil).add(subject);
 		}
 		
-		if (studentsSubjects.containsKey(pupil)) {
-			ArrayList<String> subjList = studentsSubjects.get(pupil);
-			if (!subjList.contains(subject)) {
-				subjList.add(subject);
-			}
-			studentsSubjects.put(pupil, subjList);
-		}
 	}
 	
 	public Iterator<String> getTeachers(String pupil) {
+		ArrayList<String> teachers = new ArrayList<>();
 		if (studentsSubjects.containsKey(pupil)) {
-			ArrayList<String> subjList = studentsSubjects.get(pupil);
-			ArrayList<String> teachersList = new ArrayList<String>();
-			for (String subjects: subjList) {
-				//subjectsTeachers.get(subjects);
-//				for (String teachOneSubj: subjectsTeachers.get(subjects)) {
-//					teachersList.add(teachOneSubj);
-//				}
-				if (subjectsTeachers.containsKey(subjects)) {
-					teachersList.addAll(subjectsTeachers.get(subjects)); // TODO test on addAll(null);
+			HashSet<String> subjects = studentsSubjects.get(pupil);
+			for (String subj: subjects) {
+				subjectsTeachers.get(subj);
+				for (String teach: subjectsTeachers.get(subj)) {
+					teachers.add(teach);
 				}
 			}
-			return teachersList.iterator();
+			return teachers.iterator();
 		}
 		return null;
 	}
 	
 	public Iterator<String> getPupils(String teacher) {
+		ArrayList<String> students = new ArrayList<>();
 		if (teachersSubjects.containsKey(teacher)) {
-			ArrayList<String> subjList = teachersSubjects.get(teacher);
-			ArrayList<String> studentsList = new ArrayList<String>();
-			for (String subjects: subjList) {
-				//subjectsTeachers.get(subjects);
-//				for (String studOneSubj: subjectsStudents.get(subjects)) {
-//					studentsList.add(studOneSubj);
-//				}
-				if (subjectsStudents.containsKey(subjects)) {
-					studentsList.addAll(subjectsStudents.get(subjects));
+			HashSet<String> subjects = teachersSubjects.get(teacher);
+			for (String subj: subjects) {
+				subjectsStudents.get(subj);
+				for (String stud: subjectsStudents.get(subj)) {
+					students.add(stud);
 				}
 			}
-			return studentsList.iterator();
+			return students.iterator();
 		}
 		return null;
 	}
 	
-	public void removeTeacher(String teacher) {
-		if (teachersSubjects.containsKey(teacher)) {
-			teachersSubjects.remove(teacher);
-			
-			Iterator<String> subjects = subjectsTeachers.keySet().iterator();
-			while(subjects.hasNext()) {
-				String subj = subjects.next();
-				if (subjectsTeachers.get(subj).contains(teacher)) {
-					subjectsTeachers.get(subj).remove(teacher);
-				}
-			}
-		}
-	}
+//	public void removeTeacher(String teacher) {
+//		
+//	}
 }
